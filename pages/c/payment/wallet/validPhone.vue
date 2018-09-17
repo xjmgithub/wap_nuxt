308e051a4cf473dc83e9f4db259a5f0be18ecc0e
shangxinbo <shangxb@startimes.com.cn>
2018-09-17 19:45:42 +0800

    设置密码

--- pages/c/payment/wallet/validPhone.vue
+++ pages/c/payment/wallet/validPhone.vue
@@ -1,19 +1,61 @@
 <template>
     <div class="container">
         <verifyTel prefix="234"></verifyTel>
+        <div class="footer" v-show="!loadStatus">
+            <mButton :disabled="false" :text="'OK'" @click="back"></mButton>
+        </div>
+        <div class="step2">
+            <div class="title">Enter SMS verification code</div>
+            <passInput></passInput>
+            <div class="footer">
+                <mButton :disabled="false" :text="'OK'" @click="back"></mButton>
+            </div>
+        </div>
+        <div class="step2 step3">
+            <div class="title">Confirm Password</div>
+            <passInput></passInput>
+            <div class="footer">
+                <mButton :disabled="false" :text="'OK'" @click="back"></mButton>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import verifyTel from '~/components/form/wallet_tel_verify'
+import mButton from '~/components/button'
+import passInput from '~/components/password'
 export default {
     layout: 'base',
     components: {
-        verifyTel
+        verifyTel,
+        mButton,
+        passInput
     }
 }
 </script>
 <style lang="less" scoped>
 .container {
-    padding:3rem 1rem;
+    padding: 3rem 1rem;
+    .step2 {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 2;
+        padding: 3rem 1rem;
+    }
+    .step3{
+        z-index: 3;
+    }
+}
+.footer {
+    position: fixed;
+    bottom: 2rem;
+    width: 16rem;
+    margin: 0 auto;
+    left: 0;
+    right: 0;
 }
 </style>
