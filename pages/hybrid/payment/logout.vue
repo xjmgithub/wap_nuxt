de06d5b23b1f3eeb2eb471206f9026d6cea1421f
shangxinbo <shangxb@startimes.com.cn>
2018-09-26 14:05:34 +0800

    add logout

---
+++ pages/hybrid/payment/logout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div @click="logout">logout</div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    methods: {
+        logout() {
+            document.cookie.split(';').forEach(function(c) {
+                document.cookie = c
+                    .replace(/^ +/, '')
+                    .replace(
+                        /=.*/,
+                        '=;expires=' + new Date().toUTCString() + ';path=/'
+                    )
+            })
+            localStorage.clear()
+            alert('logout success')
+        }
+    }
+}
+</script>
