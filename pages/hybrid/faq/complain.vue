6c649ca25a78ae537e174250ab182307eb03fd92
naheya <nahy@startimes.com.cn>
2018-10-08 17:06:45 +0800

    faq 部分页面基本排版

---
+++ pages/hybrid/faq/complain.vue
@@ -0,0 +1,182 @@
+<template>
+    <div id="wrapper">
+        <div class="order-msg">
+            <p class="time">9 Dec 2018 19:34:35</p>
+            <div class="order-type clearfix">
+                <img src="~assets/img/faq/ic_RechargeOrder_def_b.png" alt="">
+                <div class="right">
+                    <p class="order-name">DVB Recharge<span>￥980 </span></p>
+                    <p class="order-status">Card No. 0213 9030 831<span>Successful</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="problem">
+            <p>Your Problem</p>
+            <input type="text" placeholder="Please select the problem you need to solve">
+            <p>Your Problem</p>
+            <textarea name="" id="" cols="35" rows="5" placeholder="To rapidly help solve the problem,please show us the screenshots of your payment"></textarea>
+        </div>
+        <div class="gap"></div>
+        <div class="personal">
+            <p>Personal Information</p>
+           <ul>
+               <li><p class="p-name">Account <span>*</span></p><p class="p-value">Huangj@startimes.con.cn</p></li>
+               <li><p class="p-name"> Country <span>*</span></p><p class="p-value">Nigeria</p></li>
+               <li><p class="p-name">Telecom Info</p><p class="p-value">Safricon 4G</p></li>
+               <li><p class="p-name">Device <span>*</span></p><p class="p-value">Tecno N2.0</p></li>
+           </ul>
+        </div>
+        <div class="submit">
+            <button class="btn">
+                SUBMIT
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+    layout: 'base',
+    head() {
+      return {
+        title: 'Online Service'
+      }
+    }
+  }
+</script>
+<style lang="less">
+body {
+    background: #fff;
+}
+.wrapper{
+    font-family: 'DINPro', Roboto, Arial, Helvetica, Sans-serif
+}
+.clearfix:after {
+    display: block;
+    visibility: hidden;
+    clear: both;
+    height: 0;
+    content: '';
+}
+.clearfix {
+    zoom: 1;
+}
+.gap{
+    background-color: #F2F2F2;
+    height:4px;
+    width:100%;
+}
+.order-msg{
+    box-shadow:0px 1px 3px 1px #dddddd;
+    border-radius: 5px;
+    padding:0.5rem;
+    width:95%;
+    margin:.5rem 2.5% 0 2.5%;
+    .time{
+        width:100%;
+        color:#AAAAAA;
+        font-size:.8rem;
+        border-bottom:1px solid #EEEEEE;
+        padding:.2rem 0;
+    }
+  }
+  .order-type{
+        padding:.7em 0;
+        img{
+            width:2.5rem;
+            height:2.5rem;
+            float:left;
+        }
+        .right{
+            margin-left: 3rem;
+        }
+        .order-name{
+            span{
+            font-weight:bold;
+            float:right;
+            }
+        }
+      .order-status{
+            font-size:.9rem;
+            color:#999999;
+            span{
+                color:#00CC33;
+                float:right;
+            }
+      }
+}
+.problem{
+    padding:0.5rem;
+    p{
+        margin: .6rem 0 .3rem 0;
+        color:#212121;
+        font-weight: bold;
+        font-size: .9rem;
+    }
+    input{
+        border:1px solid #DDDDDD;
+        width:100%;
+        font-size:.8rem; 
+        padding:.3rem;
+    }
+    textarea{
+        font-size:.8rem; 
+        border:1px solid #DDDDDD;
+        width:100%;
+        padding:.3rem;
+    }
+}
+.personal{
+    width:95%;
+    margin:0 2.5%;
+    &>p{
+        margin: .6rem 0 .3rem 0;
+        color:#212121;
+        font-weight: bold;
+        font-size: .9rem;
+    }
+    ul{
+        box-shadow:0px 1px 3px 1px #dddddd;
+        padding:0.5rem 0.8rem;
+        border-radius: 5px;
+        li{
+            padding:.5rem 0;
+            p{
+                display:inline-block;
+                color:#212121;
+                font-size: .8rem;
+                &.p-name{
+                    width:30%;
+                    border-right:1px solid #eeeeee;
+                    span{
+                        color:#0087e0;
+                    }
+                }
+                &.p-value{
+                    font-weight:bold;
+                    margin-left:1.5rem;
+                }
+            }
+            &+li{
+                border-top:1px solid #eeeeee;
+            }
+        } 
+    }
+}
+.submit{
+  width:100%;
+  text-align: center;
+  color:#0087EB;
+  border-top:1px solid #eeeeee;
+  margin-top:1.5rem;
+  padding:1rem 0;
+  button{
+    margin:0 auto;
+    border:1px solid #0087EB;
+    border-radius: 2px;
+    background: #FFF;
+    padding:.3rem;
+    font-weight:bold;
+    width:60%;
+  }
+}
+</style>
---
+++ pages/hybrid/faq/onlineService.vue
@@ -0,0 +1,230 @@
+<template>
+  <div>
+    <div id="wrapper">
+    <div class="order-msg">
+        <div class="top">
+            <p class="time">9 Dec 2018 19:34:35</p>
+            <div class="order-type clearfix">
+                <img src="~assets/img/faq/ic_RechargeOrder_def_b.png" alt="">
+                <div class="right">
+                <p class="order-name">DVB Recharge<span>￥980 </span></p>
+                <p class="order-status">Card No. 0213 9030 831<span>Successful</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="gap"></div>
+        <div class="bottom clearfix">
+            <p class="clearfix">Questions <img src="~assets/img/faq/ic_categary_copy41.png" alt=""></p>
+            <ul>
+                <li>1. Paid, But recharge can’t be finished.  </li>
+                <li>2. I’ve recharged, But I still can’t watch channels on TV.</li>
+                <li>3. I’ve recharged, But I still can’t watch channels in App.</li>
+            </ul>
+            <div class="btn">COMPLAIN</div>
+        </div>
+        <div class="gap"></div>
+        <div class="more">
+            MORE ORDERS
+        </div>
+    </div>
+    <div class="service">
+      <nav id="nav">
+        <a :class="{on:serviceIndex == 1}" @click="serviceIndex = 1"><img src="~assets/img/faq/ic_categary_copy42.png" alt=""></a>
+        <a :class="{on:serviceIndex == 2}" @click="serviceIndex = 2"><img src="~assets/img/faq/ic_categary_copy2.png" alt=""></a>
+        <a :class="{on:serviceIndex == 3}" @click="serviceIndex = 3"><img src="~assets/img/faq/ic_categary_copy21.png" alt=""></a>
+        <a :class="{on:serviceIndex == 4}" @click="serviceIndex = 4"><img src="~assets/img/faq/ic_categary_copy4.png" alt=""></a>
+        <a :class="{on:serviceIndex == 5}" @click="serviceIndex = 5"><img src="~assets/img/faq/ic_tv1.png" alt=""></a>
+      </nav>
+      <div class="questions">
+        <ul v-show="serviceIndex == 1">
+          <li>Certain channels are not playing but display loading, what do…</li>
+          <li>Paid successfully, why I still can not view the premium chann…</li>
+          <li>How can I link if I am a StarTimes decoder user?</li>
+          <li>What are the accepted payment methods for payment?</li>
+          <li>What are StarTimes App live TV channels?</li>
+          <li>What should I do if the player tells me the video can’t be play…</li>
+          <li>How to subscribe StarTimes ON?</li>
+          <li>Can I download the videos to watch them offline?</li>
+          <li>Who can get the free online video streaming service?</li>
+        </ul>
+        <div v-show="serviceIndex == 2" >2</div>
+        <div v-show="serviceIndex == 3" >3</div>
+        <div v-show="serviceIndex == 4" >4</div>
+        <div v-show="serviceIndex == 5" >5</div>
+      </div>
+    </div>
+    
+  </div>
+    <div class="costomer">
+      <button class="btn">
+        COSTOMER SERVICE
+      </button>
+    </div>
+  </div>
+  
+</template>
+<script>
+  export default {
+    layout: 'base',
+    data:function(){
+      return {
+        serviceIndex:1
+      }
+    },
+    head() {
+      return {
+        title: 'Online Service'
+      }
+    }
+  }
+</script>
+<style lang="less">
+body {
+    background: #fff;
+}
+
+#wrapper{
+    padding:0.5rem;
+    font-family: 'DINPro', Roboto, Arial, Helvetica, Sans-serif
+}
+.clearfix:after {
+    display: block;
+    visibility: hidden;
+    clear: both;
+    height: 0;
+    content: '';
+}
+.clearfix {
+    zoom: 1;
+}
+
+.order-msg{
+  box-shadow:0px 1px 3px 1px #dddddd;
+  border-radius: 5px;
+  .top{
+    padding:0 0.5rem;
+     p.time{
+      width:100%;
+      color:#AAAAAA;
+      font-size:.8rem;
+      border-bottom:1px solid #EEEEEE;
+      padding:.2rem 0;
+    }
+  }
+  .order-type{
+      padding:.7em 0;
+      img{
+        width:2.5rem;
+        height:2.5rem;
+        float:left;
+      }
+      .right{
+        margin-left: 3rem;
+      }
+      .order-name{
+        span{
+          font-weight:bold;
+          float:right;
+        }
+      }
+      .order-status{
+        font-size:.9rem;
+        color:#999999;
+        span{
+          color:#00CC33;
+          float:right;
+        }
+      }
+  }
+  .gap{
+    background-color: #F2F2F2;
+    height:4px;
+    width:100%;
+  }
+  .bottom{
+    padding:.7rem 0.5rem;
+    p{
+      font-size:.9rem;
+      color:#333333;
+      img{
+        float:right;
+        width:1.5rem;
+        height:1.5rem;
+      }
+    }
+    li{
+      color:#333333;
+      font-size:.8rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap; 
+    }
+    .btn{
+      color:#0087EB;
+      font-size:.8rem;
+      margin-top:.7rem;
+      float:right;
+      font-weight:bold;
+    }
+  }
+  .more{
+    color:#0087EB;
+    font-size:.8rem;
+    font-weight:bold;
+    text-align: center;
+    padding:.7rem 0;
+  }
+}
+.service{
+  margin-top:1rem;
+  #nav {
+    width:100%;
+    margin:0 auto;
+    text-align:center;
+    padding-bottom:.7rem;
+    a{
+      border-bottom:3px solid #EEEEEE;
+      width: 20%;
+      display: inline-block;
+      padding-bottom:.7rem;
+      &.on{
+        border-bottom:3px solid #0087EB;
+      }
+      img{
+      width:1.5rem;
+      height:1.5rem;
+      }
+    }
+  }
+  .questions{
+    li{
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap; 
+      padding:.5rem 0;
+      color:#333333;
+      font-size: .8rem;
+      &+li{
+        border-top:1px solid #eeeeee;
+      }
+    }
+  }
+}
+.costomer{
+  width:100%;
+  text-align: center;
+  color:#0087EB;
+  border-top:1px solid #eeeeee;
+  margin-top:1.5rem;
+  padding:1rem 0;
+  button{
+    margin:0 auto;
+    border:1px solid #0087EB;
+    border-radius: 2px;
+    background: #FFF;
+    padding:.3rem;
+    font-weight:bold;
+    width:60%;
+  }
+}
+</style>
