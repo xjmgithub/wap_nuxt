00fa518b87f03cea2499bb159414526ea1ea8c29
shangxinbo <shangxb@startimes.com.cn>
2018-10-09 17:51:10 +0800

    payment

--- env.js
+++ env.js
@@ -1,10 +1,10 @@
 module.exports = {
-    ga_wap_key: 'UA-55969660-4',
-    ga_android_key: 'UA-55969660-7',
-    ga_ios_key: 'UA-55969660-5',
-    countly_host: '//collect.startimestv.com',
-    countly_appKey: 'cd58cfb3e81658905d802cbe44b04683effbc7ed',
-    gtbank_jdk_host: 'https://eu-gateway.mastercard.com',
-    ms_host:'http://staging.upms.startimestv.com/',
-    mechant_request_url:'http://172.31.46.79:8010/'
+    ga_wap_key: 'UA-55969660-8',
+    ga_android_key: 'UA-55969660-8',
+    ga_ios_key: 'UA-55969660-8',
+    countly_host: '//10.0.63.102:32769',
+    countly_appKey: 'cb95b4e9125098b99a7df2fbda4554eda982918b',
+    gtbank_jdk_host: 'https://test-gateway.mastercard.com',
+    ms_host:'http://qa.upms.startimestv.com/',
+    mechant_request_url:'http://10.0.63.5:8010/'
 }
