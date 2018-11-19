2c1deb66be27edcddbe7f18f209b0fe11ae85c8e
shangxinbo <shangxb@startimes.com.cn>
2018-11-19 17:46:43 +0800

    message 详情页

--- pages/hybrid/faq/message.vue
+++ pages/hybrid/faq/message.vue
@@ -1,38 +1,39 @@
 <template>
     <div id="wrapper">
-        <div class="order-msg" v-if="order.order_status">
-            <p class="time">{{order.order_create_time | formatDate }}
-                <nuxt-link :to="{'path':'/hybrid/faq/chooseOrder',query:$route.query}">
-                    <img src="~assets/img/faq/ic_Setting_def_g.png" alt="">
-                </nuxt-link>
-            </p>
+        <div class="reply">
+            <div class="waiting" v-msg="!msg.replyRecordDtoList||msg.replyRecordDtoList.length<=0">
+                <div class="waiting_btn">Waiting For Result…</div>
+            </div>
+            <div class="over" v-msg="msg.replyRecordDtoList&&msg.replyRecordDtoList.length>0">
+                <div class="replied_txt">Replied</div>
+                <div class="replied_content">
+                    Hi, We have found the problem. Your account hasn’t changed correctly. We’re sorry for our mistake.
+                </div>
+                <div class="replied_content">
+                    ASDASDF unt hasn’t changed correctly. We’re sorry for our mistake.
+                </div>
+            </div>
+        </div>
+        <div class="gap"></div>
+        <div class="order-msg" v-if="msg.order_status">
+            <p class="time">{{msg.orderCreateTime | formatDate }}</p>
             <div class="order-type clearfix">
                 <img src="~/assets/img/faq/ic_RechargeOrder_def_b.png" alt="">
                 <div class="right">
-                    <p class="order-name">{{order.order_type }}
-                        <span>{{order.order_amount }}</span>
+                    <p class="order-name">{{msg.orderType }}
+                        <span>{{msg.orderAmount }}</span>
                     </p>
-                    <p class="order-status">{{order.card_no }}
-                        <span>{{order.order_status }}</span>
+                    <p class="order-status">{{msg.orderName }}
+                        <span>{{msg.orderStatus }}</span>
                     </p>
                 </div>
             </div>
         </div>
-        <div class="choose-order" v-else>
-            <nuxt-link to="/hybrid/faq/chooseOrder">
-                <div>
-                    <img src="~/assets/img/faq/ic_add_def_g.png"> Choose An Order
-                </div>
-            </nuxt-link>
-        </div>
         <div class="problem">
             <p>Your Problem</p>
-            <mselect :list="questionsList" :default="question" placeholder="Please choose your question" ref="questionSelect" @change="setQuestion"></mselect>
-            <mselect :list="channelList" placeholder="Please choose channel type" ref="channelSelect" @change="setChannelName" v-if="type[1]"></mselect>
-            <mselect :list="channelNameList" placeholder="Please choose channel name" ref="channelNameSelect" v-if="type[1]"></mselect>
-            <mselect :list="countryList" :default="defaultCountry" placeholder="Please choose your country" ref="countrySelect" v-if="!type[0]&&!type[1]"></mselect>
+            <div>{{msg.problem}}</div>
             <p>Detail Description</p>
-            <textarea cols="35" rows="5" placeholder="To rapidly help solve the problem,please show us the screenshots of your payment" v-model="moredes"></textarea>
+            <div class="msg_container">{{msg.message}}</div>
         </div>
         <div class="gap"></div>
         <div class="personal">
@@ -42,223 +43,44 @@
                     <p class="p-name">Account
                         <span>*</span>
                     </p>
-                    <p class="p-value">{{user.id}}</p>
+                    <p class="p-value">{{msg.Account}}</p>
                 </li>
                 <li>
                     <p class="p-name">Country
                         <span>*</span>
                     </p>
-                    <p class="p-value">{{user.countryCode}}</p>
+                    <p class="p-value">{{msg.countryCode}}</p>
                 </li>
                 <li v-if="carrier">
                     <p class="p-name">Telecom Info
                         <span>*</span>
                     </p>
-                    <p class="p-value">{{carrier}}</p>
+                    <p class="p-value">{{msg.operatorInfo}}</p>
                 </li>
                 <li v-if="unitType">
                     <p class="p-name">Device
                         <span>*</span>
                     </p>
-                    <p class="p-value">{{unitType}}</p>
+                    <p class="p-value">{{msg.unitType}}</p>
                 </li>
             </ul>
-        </div>
-        <div class="submit">
-            <button class="btn" @click="submit">SUBMIT</button>
         </div>
     </div>
 </template>
 <script>
-import mselect from '~/components/select'
 import moment from 'moment/moment.js'
 export default {
     layout: 'base',
     data() {
         return {
-            indexId: 'Select Channel Type',
-            indexId2: 'Select Channel Name',
-            indexNum: 0,
-            order: {},
-            user: this.$store.state.user,
-            carrier: this.$store.state.carrier,
-            unitType: this.$store.state.phoneModel,
-            type: [0, 0],
-            questionsList: [],
-            question: '',
-            channelList: [],
-            channelNameList: [],
-            countryList: [],
-            defaultCountry: '',
-            moredes: ''
-        }
-    },
-    methods: {
-        setChannelName(item) {
-            this.channelNameList = item.disparkChannel
-        },
-        setQuestion(question) {
-            let tags = question.tags
-            let type = [0, 0] // 1 支付，2频道
-            if (tags && tags.length > 0) {
-                tags.forEach(item => {
-                    if (item.tagging_name == 'pay') {
-                        type[0] = 1
-                    }
-                    if (item.tagging_name == 'channel') {
-                        type[1] = 1
-                    }
-                })
-            }
-            this.type = type
-        },
-        submit() {
-            let order = localStorage.getItem('orderMsg')
-
-            if (this.type[1]) {
-                if (!this.$refs.channelSelect.selected.id) {
-                    this.$alert('Please select a channel type')
-                    return false
-                }
-                if (!this.$refs.channelNameSelect.selected.id) {
-                    this.$alert('Please select a channel Name')
-                    return false
-                }
-            }
-            if (!this.type[0] && !this.type[1]) {
-                if (!this.$refs.countrySelect.selected.id) {
-                    this.$alert('Please select a country')
-                    return false
-                }
-            }
-
-            if (!this.moredes) {
-                this.$alert('Please fill in the problem description')
-                return false
-            }
-
-            let param = {
-                orderType: order ? JSON.parse(order).order_type_id : '',
-                orderNo: order ? JSON.parse(order).order_no : '',
-                userAccount: this.user.userName,
-                unitType: this.unitType || '',
-                operatorInfo: this.carrier || '',
-                problemId: this.$refs.questionSelect.selected.id,
-                problem: this.$refs.questionSelect.selected.name,
-                problemChannelTypeKey: this.type[1]
-                    ? this.$refs.channelSelect.selected.id
-                    : '',
-                problemChannelTypeValue: this.type[1]
-                    ? this.$refs.channelSelect.selected.name
-                    : '',
-                problemChannelNameKey: this.type[1]
-                    ? this.$refs.channelNameSelect.selected.id
-                    : '',
-                problemChannelNameValue: this.type[1]
-                    ? this.$refs.channelNameSelect.selected.name
-                    : '',
-                problemCountryId:
-                    !this.type[0] && !this.type[1]
-                        ? this.$refs.countrySelect.selected.id
-                        : '',
-                problemCountryCode:
-                    !this.type[0] && !this.type[1]
-                        ? this.$refs.countrySelect.selected.name
-                        : '',
-                message: this.moredes,
-                channelNameAdditional: '',
-                channelType: ''
-            }
-
-            this.$axios
-                .post(
-                    `/csms-service/v1/standard-leaving-message-records`,
-                    param
-                )
-                .then(res => {
-                    if (res.data.code == 0) {
-                        localStorage.setItem(
-                            'leaveMsg',
-                            Object.assign({}, param, {
-                                id: res.data.data.messageId
-                            })
-                        )
-                        this.$router.push('/hybrid/faq/customerService')
-                    }
-                })
+            msg: {}
         }
     },
     mounted() {
-        let order = localStorage.getItem('orderMsg')
-        if (order) {
-            this.order = JSON.parse(order)
-        } else {
-            this.order = {}
+        let msg = localStorage.getItem('showMsg')
+        if (msg) {
+            this.msg = JSON.parse(msg)
         }
-
-        let ua = window.navigator.userAgent
-        let deviceInfo = ua.match(/\(([^)]*)\)/)[1].split(';')
-        this.deviceInfo = deviceInfo[deviceInfo.length - 1].split('like')[0]
-
-        // more faqs
-        let serviceModuleId = localStorage.getItem('serviceModuleId')
-
-        // 如果是从首页的单个faq默认选中
-        let faq_question = localStorage.getItem('faq_question')
-        this.$axios
-            .get(`/ocs/v1/moreFaqs?serviceModuleId=${serviceModuleId}`)
-            .then(res => {
-                if (res.data.code == 200) {
-                    let list = []
-                    res.data.data.forEach((item, index) => {
-                        list.push({
-                            id: item.id,
-                            name: item.thema,
-                            tags: item.tags
-                        })
-                    })
-                    this.questionsList = list
-                    if (faq_question) {
-                        this.question = JSON.parse(faq_question).id
-                    }
-                }
-            })
-
-        // 渠道分类
-        this.$axios.get(`/cms/vup/channels/dispark/categories`).then(res => {
-            if (res.data && res.data instanceof Array) {
-                res.data.forEach(item => {
-                    item.disparkChannel.forEach(i => {
-                        i.id = i.channelId
-                    })
-                })
-                this.channelList = res.data
-            }
-        })
-
-        // country
-        this.$axios
-            .get(
-                `/cms/vup/v2/areas?versionCode=${this.$store.state.appVersion}`
-            )
-            .then(res => {
-                if (res.data && res.data instanceof Array) {
-                    this.countryList = res.data
-                    this.countryList.forEach(item => {
-                        if (item.country) {
-                            if (
-                                item.country.toLowerCase() ==
-                                this.user.countryCode.toLowerCase()
-                            ) {
-                                this.defaultCountry = item.id
-                            }
-                        }
-                    })
-                }
-            })
-    },
-    components: {
-        mselect: mselect
     },
     filters: {
         formatDate(date) {
@@ -309,26 +131,6 @@ export default {
     }
 }
 
-.choose-order {
-    box-shadow: 0px 1px 3px 1px #dddddd;
-    padding: 1.3rem;
-    width: 95%;
-    margin: 0.5rem 2.5% 0 2.5%;
-    border-radius: 2px;
-    div {
-        width: 100%;
-        text-align: center;
-        color: #212121;
-        font-size: 0.85rem;
-        font-weight: bold;
-        img {
-            vertical-align: sub;
-            margin-right: 0.2rem;
-            width: 0.9rem;
-        }
-    }
-}
-
 .problem {
     padding: 0.5rem;
     p {
@@ -336,21 +138,6 @@ export default {
         color: #212121;
         font-weight: bold;
         font-size: 0.9rem;
-    }
-    select {
-        border: 1px solid #dddddd;
-        width: 100%;
-        font-size: 0.8rem;
-        padding: 0.3rem;
-        margin-bottom: 0.3rem;
-        border-radius: 4px;
-    }
-    textarea {
-        font-size: 0.8rem;
-        border: 1px solid #dddddd;
-        width: 100%;
-        padding: 0.3rem;
-        border-radius: 2px;
     }
 }
 
@@ -392,27 +179,26 @@ export default {
     }
 }
 
-.submit {
-    width: 100%;
-    text-align: center;
-    color: #0087eb;
-    border-top: 1px solid #eeeeee;
-    margin-top: 1.5rem;
-    padding: 1rem 0;
-    position: fixed;
-    bottom: 0;
-    height: 4.25rem;
-    background: #fff;
-
-    button {
-        margin: 0 auto;
-        border: 1px solid #0087eb;
-        border-radius: 2px;
-        background: #fff;
-        padding: 0.3rem;
+.reply {
+    padding: 0.5rem;
+    .replied_txt {
         font-weight: bold;
-        width: 60%;
-        outline: none;
+        padding-top: 0.5rem;
+    }
+    .replied_content {
+        border: solid 1px #dddddd;
+        padding: 0.5rem;
+        font-size: 0.9rem;
+        border-radius: 0.2rem;
+        margin: 0.3rem 0;
+    }
+    .waiting_btn {
+        background: #dddddd;
+        border-radius: 0.2rem;
+        height: 2.2rem;
+        line-height: 2.2rem;
+        text-align: center;
+        color: #aaaaaa;
     }
 }
 </style>
