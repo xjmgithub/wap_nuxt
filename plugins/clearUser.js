86617cb076fbc6a3a2f68512b0e73c77fc87efd0
shangxinbo <shangxb@startimes.com.cn>
2018-11-26 10:25:51 +0800

    跳转登录

--- plugins/clearUser.js
+++ plugins/clearUser.js
@@ -1,5 +1,5 @@
 
-import {setCookie} from '~/functions/utils'
+import {setCookie,toNativePage} from '~/functions/utils'
 export default ({ app: { router }, store }) => {
     router.beforeEach((to, from, next) => {
         // 清除计时器
@@ -9,8 +9,19 @@ export default ({ app: { router }, store }) => {
         if (store.state.user) {
             next()
         } else {
-            setCookie('token','') // 清除失效的token
-            location.reload()
+            // 如果是app里
+            if(store.state.appType==1||store.state.appType==2){
+                window.alert('your account is signed in elsewhere')
+                if (store.state.appType == 1) {
+                    toNativePage('com.star.mobile.video.account.LoginActivity')
+                } else {
+                    toNativePage('startimes://login')
+                }
+            }else{
+                setCookie('token','') // 清除失效的token
+                location.reload()
+            }
+            
         }
         
     })
