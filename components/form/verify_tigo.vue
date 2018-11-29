ec92e9f1ca24a0b5fcf7d0dac216bf25e6837cd7
naheya <nahy@startimes.com.cn>
2018-11-29 13:05:26 +0800

    tigo

---
+++ components/form/verify_tigo.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <p class="label">{{input_label}}</p>
+        <div class="input-tel" >
+            <div class="prefix">{{prefix}}</div>
+            <div class="number">
+                <input
+                    type="Number"
+                    v-model="tel"
+                    placeholder="9 digits total"
+                >
+            </div>
+            <div class="error" v-show="error_tel">{{error_tel}}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        prefix: {
+            type: String,
+            required: true
+        },
+        input_label: {
+            type: String,
+            default: ""
+        },
+        error_tel: {
+            type: String,
+            default: ""
+        }
+    },
+    data() {
+        return {
+            tel:'',
+        }
+    },
+    watch:{
+        tel(nv ,ov) {
+            this.error_tel = ''
+            if( nv.length > 9) {
+                this.tel = nv.slice(0,9)
+            }
+        }
+    }
+
+}
+</script>
+<style lang="less" scoped>
+.label{
+    font-size:1.2rem;
+}
+.input-tel {
+    border-bottom: #dddddd solid 1px;
+    display: -webkit-box;
+    display: flex;
+    padding-bottom: 5px;
+    margin: 1rem 0 2rem;
+    position: relative;
+    .prefix {
+        max-width: 3.5rem;
+        border-right: #dddddd solid 1px;
+        line-height: 1.5rem;
+        height: 1.5rem;
+        float: left;
+        -webkit-box-flex: 1;
+        flex: 1;
+    }
+    .number {
+        -webkit-box-flex: 11;
+        flex: 5;
+        input {
+            width: 100%;
+            border: none;
+            display: block;
+            padding: 0 0.5rem;
+            height: 1.5rem;
+            line-height: 1.5rem;
+            outline: none;
+        }
+    }
+    .error {
+        position: absolute;
+        bottom: -1.4rem;
+        font-size: 0.8rem;
+        color: red;
+    }
+}
+</style>
---
+++ pages/hybrid/parket/tigo.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="wrapper">
+        <div class="by_tel">
+            <verifyTigo ref="telpicker" :prefix="phonePrefix" :error_tel="error_tel" :input_label="input_label"/>
+        </div>
+        <div class="footer">
+            <mButton :disabled="false" :text="'OK'" @click="nextStep"/>
+        </div>
+    </div>
+</template>
+<script>
+import verifyTigo from '~/components/form/verify_tigo'
+import mButton from '~/components/button'
+ export default {
+      layout: 'base',
+      data(){
+          return {
+            phonePrefix:"255 0",
+            error_tel:'',
+            input_label:'Input phone number to continue'
+          }
+      },
+      methods: {
+          nextStep(){
+                let phone = this.$refs.telpicker.tel
+                console.log(phone)
+                if(phone.length < 9){
+                    this.error_tel = 'Must be 9 digits'
+                    console.log(111)
+                }else{
+                    window.location.href = 'http://www.tigosports.co.tz/Home/Home?MSISDN=255' + phone + '&ConsumerName=Startimes'
+                }
+          }
+      },
+      head() {
+        return {
+            title: 'Tigo'
+        }
+    },
+     components: {
+        verifyTigo,
+        mButton
+    },
+ }   
+</script>
+<style   lang="less" scoped>
+#wrapper {
+    padding: 1rem 0.8rem;
+    .by_tel {
+        padding-top: 5rem;
+    }
+    .footer {
+        position: fixed;
+        bottom: 2rem;
+        width: 75%;
+        margin: 0 auto;
+        left: 0;
+        right: 0;
+    }
+}
+</style>
