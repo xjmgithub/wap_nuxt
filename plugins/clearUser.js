b78df5f0109468b66874ec7d273bc73749646c47
shangxinbo <shangxb@startimes.com.cn>
2018-12-17 15:35:34 +0800

    跳转

--- plugins/clearUser.js
+++ plugins/clearUser.js
@@ -5,18 +5,16 @@ export default ({ app: { router }, store }) => {
         if (store.state.intervalTimer) {
             clearInterval(store.state.intervalTimer)
         }
-        console.log(store.state.user)
         if (store.state.user) {
             next()
         } else {
             // 如果是app里
-            next('/hybrid/account/tokenfail')
-            // if (store.state.appType == 1 || store.state.appType == 2) {
-            //     next('/hybrid/account/tokenfail')
-            // } else {
-            //     setCookie('token', '') // 清除失效的token
-            //     location.reload()
-            // }
+            if (store.state.appType == 1 || store.state.appType == 2) {
+                next('/hybrid/account/tokenfail')
+            } else {
+                setCookie('token', '') // 清除失效的token
+                location.reload()
+            }
         }
     })
 }
