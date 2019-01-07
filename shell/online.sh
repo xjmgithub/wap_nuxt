9ef367c3997efeea0713c215a1bdb622d76cb1af
shangxinbo <shangxb@startimes.com.cn>
2019-01-07 13:32:58 +0800

    发布脚本

---
+++ shell/online.sh
@@ -0,0 +1,24 @@
+source /root/.bashrc
+
+tar -zvxf dist.tar.gz
+yes | cp -rf configs/production.js env.js
+
+if $1 eq true
+then
+        npm config set registry https://registry.npm.taobao.org
+        npm install
+fi
+
+
+if [ "$2" = "" ]
+then
+        echo 'version commont needed'
+        exit 1
+else
+        npm run build
+        { # try
+                pm2 restart wap_nuxt
+        }||{ # catch
+                pm2 start /root/.nvm/versions/node/v10.14.2/bin/npm --name="wap_nuxt" -- run start
+        }
+fi
