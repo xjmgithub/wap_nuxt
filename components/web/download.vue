8ca426c104a0f0c2914cb7aa9421c57500fcaef0
shangxinbo <shangxb@startimes.com.cn>
2019-01-14 17:02:54 +0800

    download link

--- components/web/download.vue
+++ components/web/download.vue
@@ -1,19 +1,23 @@
 <template>
-    <nuxt-link to="/hybrid/lands">
-        <p class="download">Download StarTimes On App to watch! 
+    <nuxt-link class="link_to_download" to="/hybrid/lands">
+        <p class="download">
+            Download StarTimes On App to watch!
             <span>GO &gt;</span>
         </p>
     </nuxt-link>
 </template>
-<style lang="less" scope>
-.download{
-    padding:.5rem .8rem;
-    background-color: #FFBE19;
-    font-weight: bold;
-    font-size: .9rem;
-    span{
-        color:#0087EB;
-        float: right;
+<style lang="less" scoped>
+.link_to_download {
+    .download {
+        padding: 0.5rem 0.8rem;
+        background-color: #ffbe19;
+        font-weight: bold;
+        font-size: 0.9rem;
+        color:#333333;
+        span {
+            color: #0087eb;
+            float: right;
+        }
     }
 }
 </style>
