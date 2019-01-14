d68d79823df76fea1b3cfc7ea1b8fb5fd55aba11
shangxinbo <shangxb@startimes.com.cn>
2019-01-14 10:17:32 +0800

    切换页面导航消失

--- plugins/clearTimer.js
+++ plugins/clearTimer.js
@@ -1,6 +1,9 @@
 import { setCookie, toNativePage } from '~/functions/utils'
 export default ({ app: { router }, store }) => {
     router.beforeEach((to, from, next) => {
+        // 路由切换侧边栏导航收回
+        store.commit('SET_NAV_STATE', false)
+
         // 清除计时器
         if (store.state.intervalTimer) {
             clearInterval(store.state.intervalTimer)
