f88d3d4c09999d9fce3e7b9ff6a3fac45431838c
shangxinbo <shangxb@startimes.com.cn>
2019-01-14 10:05:45 +0800

    国家语言切换

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,17 @@
         <div class="slide-bar">
             <ul>
                 <li>Signin / Register</li>
-                <li>language</li>
-                <li>Country</li>
-                <li>FAQ</li>
-                <li>Contact Us</li>
+                <li class="country">
+                    <span>{{country.name}}</span>
+                    <img :src="country.nationalFlag">
+                </li>
+                <li>{{language}}</li>
+                <li>
+                    <a href="https://m.startimestv.com/faq.php">FAQ</a>
+                </li>
+                <li>
+                    <a href="https://m.startimestv.com/business.php">Contact Us</a>
+                </li>
             </ul>
         </div>
     </div>
@@ -16,9 +23,25 @@
 <script>
 import mheader from '~/components/web/header.vue'
 export default {
-    computed:{
-        showNav(){
+    computed: {
+        showNav() {
             return this.$store.state.navState
+        },
+        country() {
+            return this.$store.state.country
+        },
+        language(){
+            let type = this.$store.state.langType
+            switch(type){
+                case 'fr': 
+                    return 'French'
+                case 'sw': 
+                    return 'Kiswahili'
+                case 'pt': 
+                    return 'português'
+                default : 
+                    return 'English'
+            }
         }
     },
     components: {
@@ -29,16 +52,16 @@ export default {
             switch (url) {
                 case 'home':
                     this.$router.push('/browser')
-                    break;
+                    break
                 case 'menu':
                     this.$router.push('/browser/menu')
-                    break;
+                    break
                 case 'tvguide':
                     this.$router.push('/browser/tvguide')
-                    break;
+                    break
                 case 'phoneplay':
                     this.$router.push('/browser/phoneplay')
-                    break;
+                    break
             }
         }
     },
@@ -54,10 +77,10 @@ body {
     margin: 0;
 }
 
-.frame{
+.frame {
     transition: transform 0.3s;
 }
-.frame.shownav{
+.frame.shownav {
     transform: translateX(10rem);
 }
 
@@ -65,17 +88,27 @@ body {
     position: absolute;
     left: -10rem;
     top: 0;
-    width:10rem;
-    background:#222527;
-    ul{
-        padding:1rem;
-        color:white;
-        line-height:2rem;
-        font-size:0.9rem;
+    width: 10rem;
+    background: #222527;
+    ul {
+        padding: 1rem;
+        color: white;
+        line-height: 2rem;
+        font-size: 0.9rem;
+        a{
+            color:white;
+        }
     }
 }
 .wrapper {
     box-shadow: -10rem 0 0 0px #222527;
-    background:white;
+    background: white;
+}
+.country{
+    img{
+        width:1.5rem;
+        height:1.5rem;
+        margin-left:0.2rem;
+    }
 }
 </style>
