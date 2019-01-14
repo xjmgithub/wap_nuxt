461f368d22a3362f2746f7c1a15e906f4d5492ac
shangxinbo <shangxb@startimes.com.cn>
2019-01-14 16:59:55 +0800

    landingpage

--- languages/sy.js
+++ languages/sy.js
@@ -2,5 +2,6 @@ export default {
     home:'Mwanzo',
     today:'Today',
     leave_msg_btn:'Leave a Message',
-    live_chat_btn:'Live Chat'
+    live_chat_btn:'Live Chat',
+    download_apk:'Kupakua'
 }
