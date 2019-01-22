eada70e5b5e52fc47a83500d3322378c5dd51809
naheya <nahy@startimes.com.cn>
2019-01-22 15:51:13 +0800

    添加视频节目预告时长

--- pages/browser/programList/program.vue
+++ pages/browser/programList/program.vue
@@ -10,7 +10,10 @@
             <p>{{$store.state.lang.officialwebsitemobile_subprogramdetails_clips}}</p>
             <ul class="clearfix">
                 <li v-for="(item,index) in subProgram" :key="index" @click="toSubProgramDetail(item)">
-                    <img :src="item.poster.resources[0].url">
+                    <div>
+                        <img :src="item.poster.resources[0].url">
+                        <span class="show-time">{{item.durationSecond | formatShowTime}}</span>
+                    </div>
                     <span>{{item.name}}</span>
                 </li>
             </ul>
@@ -53,6 +56,23 @@ export default {
         toSubProgramDetail(item){
             sessionStorage.setItem('subprogram',JSON.stringify(item))
             this.$router.push('/browser/programlist/subProgram')
+        }
+    },
+    filters:{
+        formatShowTime(val){
+            if(val<60){
+                let tmp = val < 10 ? '0'+val : val
+                return '00:'+ val
+            }else if(val>=60 && val<360){
+                let min = Math.floor(val / 60) < 10 ? '0'+ Math.floor(val / 60) : Math.floor(val / 60)
+                let sec = Math.floor(val % 60) < 10 ? '0'+ Math.floor(val % 60) : Math.floor(val % 60)
+                return min + ':' + sec
+            }else if(val>=360){
+                let hour = Math.floor(val / 360) < 10 ? '0'+ Math.floor(val / 360) : Math.floor(val / 360)
+                let min = Math.floor(val % 360 / 60) < 10 ? '0'+ Math.floor(val % 360 / 60) : Math.floor(val % 360 / 60)
+                let sec = Math.floor(val % 60) < 10 ? '0'+ Math.floor(val % 60) : Math.floor(val % 60)
+                return hour +':' + min + ':' + sec
+            }
         }
     },
     components:{
@@ -110,10 +130,22 @@ export default {
                 &:nth-child(2n){
                     float: right;
                 }
-                img{
+                div {
+                    position: relative;
+                    .show-time{
+                        position: absolute;
+                        bottom:0;
+                        right:0;
+                        padding:0 .2rem;
+                        background:rgba(0,0,0,1);
+                        color:#FFFFFF;
+                        font-size: .8rem;
+                    }
+                    img{
                     width:100%;
                     display: block;
                     height:5rem;
+                    }
                 }
                 span{
                     font-size: .85rem;
