a58e7c643e4bac430955de35ae7ba25c77e7d559
shangxinbo <shangxb@startimes.com.cn>
2019-03-21 12:00:41 +0800

    loading

--- pages/browser/hall.vue
+++ pages/browser/hall.vue
@@ -4,9 +4,17 @@
             <ul v-if="hallList">
                 <li v-for="(item,i) in hallList " :key="i">
                     <p>{{item.name}}</p>
-                    <p v-if="i==0"><a :href="'tel:'+item.phone">{{item.phone}}</a> {{item.des}}</p>
-                    <p v-else><a :href="'tel:'+item.phone">{{item.phone}}</a></p>
-                    <p v-if="i==0"><a :href="'tel:'+item.phone1">{{item.phone1}}</a> {{item.des1}}</p>
+                    <p v-if="i==0">
+                        <a :href="'tel:'+item.phone">{{item.phone}}</a>
+                        {{item.des}}
+                    </p>
+                    <p v-else>
+                        <a :href="'tel:'+item.phone">{{item.phone}}</a>
+                    </p>
+                    <p v-if="i==0">
+                        <a :href="'tel:'+item.phone1">{{item.phone1}}</a>
+                        {{item.des1}}
+                    </p>
                     <p v-else>{{item.address}}</p>
                 </li>
             </ul>
@@ -15,55 +23,56 @@
 </template>
 <script>
 export default {
-    data(){
-        return{
-           hallList:[] ,
-           country: this.$store.state.country,
+    data() {
+        return {
+            hallList: [],
+            country: this.$store.state.country
         }
     },
     mounted() {
         const id = this.$route.query.regionId
         const national = this.country.phoneNumber.split('@')
         const nationalLine = {
-            name:'National Service Hotline',
-            phone:national[0].split(',')[0],
-            des:national[0].split(',')[1],
-            phone1:national[1].split(',')[0],
-            des1:national[1].split(',')[1]
-        } 
+            name: 'National Service Hotline',
+            phone: national[0].split(',')[0],
+            des: national[0].split(',')[1],
+            phone1: national[1] ? national[1].split(',')[0] : '',
+            des1: national[1] ? national[1].split(',')[1] : ''
+        }
         this.$axios.get(`/cms/areas/callcenter?regionId=${id}`).then(res => {
-           this.hallList = res.data
-           this.hallList.unshift(nationalLine)
+            this.$nextTick(() => this.$nuxt.$loading.finish())
+            this.hallList = res.data
+            this.hallList.unshift(nationalLine)
         })
     }
 }
 </script>
 <style  lang="less" scoped>
-.hall{
+.hall {
     width: 100%;
     padding: 0 0.8rem;
-    li{
+    li {
         list-style: none;
-        padding:1rem 0 1rem 1rem;
+        padding: 1rem 0 1rem 1rem;
         font-size: 1.1rem;
-        border-bottom: 1px solid #D8D8D8;
-        font-size: .95rem;
-        p{
-            color:#333333;
-            &:nth-child(1){
+        border-bottom: 1px solid #d8d8d8;
+        font-size: 0.95rem;
+        p {
+            color: #333333;
+            &:nth-child(1) {
                 font-size: 1.1rem;
                 font-weight: bold;
-                margin-bottom:1rem; 
+                margin-bottom: 1rem;
             }
-            &:nth-child(2){
-                margin-bottom:.7rem; 
+            &:nth-child(2) {
+                margin-bottom: 0.7rem;
             }
-            a{
-                color:#0087EB;
+            a {
+                color: #0087eb;
                 text-decoration: underline;
             }
         }
-        img{
+        img {
             width: 1.5rem;
             height: 1.5rem;
             float: right;
