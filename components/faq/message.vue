bb7a62f9c8b080c56a1aa7e574fb1d5302ad9dae
shangxinbo <shangxb@startimes.com.cn>
2019-04-01 14:20:59 +0800

    ott订单的显示

--- components/faq/message.vue
+++ components/faq/message.vue
@@ -53,7 +53,7 @@ export default {
                 case 3:
                     return this.order.order_type || 'DVB Recharge'
                 default:
-                    return this.order.order_name
+                    return 'StarTimes ON ' + this.order.order_name
             }
         },
         orderNo() {
