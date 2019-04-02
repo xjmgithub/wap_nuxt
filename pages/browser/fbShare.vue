31bbecac25f9cea6b4cf6d9cc50292e068a68988
naheya <nahy@startimes.com.cn>
2019-04-02 14:39:13 +0800

    test

--- pages/browser/fbShare.vue
+++ pages/browser/fbShare.vue
@@ -4,37 +4,34 @@
             <h1>Share Dialog</h1>
         </div>
         <p>Click the button below to trigger a Share Dialog</p>
-        <div class="shareBtn" @click="fbshare">Share</div>
+        <div id="shareBtn" class="shareBtn">Share</div>
     </div>
 </template>
 <script>
+import { initFacebookLogin } from '~/functions/utils'
 export default {
     layout: 'base',
-    methods: {
-        fbshare() {
-            FB.ui(
-                {
-                    method: 'share',
-                    display: 'popup',
-                    href: 'http://m.startimestv.com/browser/program/subdetail/3816?subId=31629'
-                },
-                function(response) {}
-            )
-        }
-    },
     mounted(){
-        this.fbshare()
+        initFacebookLogin()
+       document.getElementById('shareBtn').onclick = function() {
+            // eslint-disable-next-line no-undef
+            FB.ui({
+                method: 'share',
+                display: 'popup',
+                href: 'http://m.startimestv.com/browser/program/subdetail/3816?subId=31629',
+            }, function(response){});
+        }
     },
     head() {
         return {
             title: 'Facebook Share',
             meta: [
-                { name: 'og:url', content: 'http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html' },
-                { name: 'og:type', content: 'article' },
-                { name: 'og:title', content: 'When Great Minds Don’t Think Alike' },
-                { name: 'og:description', content: 'How much does culture influence creative thinking?' },
+                { property: 'og:url', content: 'http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html' },
+                { property: 'og:type', content: 'article' },
+                { property: 'og:title', content: 'When Great Minds Don’t Think Alike' },
+                { property: 'og:description', content: 'How much does culture influence creative thinking?' },
                 {
-                    name: 'og:image',
+                    property: 'og:image',
                     content: 'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg'
                 }
             ]
