9e4c9893a43b422b8d6ef91060738acb5f6f0c50
naheya <nahy@startimes.com.cn>
2019-04-16 15:47:48 +0800

    部分布局及部分接口调试

--- pages/hybrid/vote/MrRight.vue
+++ pages/hybrid/vote/MrRight.vue
@@ -10,61 +10,95 @@
         </div>
         <div class="vote">
             <p>
-                <span class="heart" />
-                <span class="title">Weekly Candidates</span>
-                <span class="voteleft">Left vote today:{{voteLeft}}</span>
+                <img class="heart" src="~assets/img/vote/heartpoint.png">
+                <span class="title">{{$store.state.lang.mrright_weekly_candidates}}</span>
+                <span class="voteleft">{{$store.state.lang.mrright_left_vote_today}} {{voteLeft}}</span>
             </p>
             <ul class="clearfix">
-                <li v-for="(item,index) in advisorList" :key="index" data-id="item.id">
+                <li v-for="(item,index) in coupleList" :key="index" :class="{'only-two':advisorList.length<3}" data-id="item.id">
                     <div class="img-box">
                         <img :src="item.icon.replace('http:','https:')" class="icon">
+                        <img src="~assets/img/vote/ic_play_small_white.png" class="player" v-show="item.link_vod_code">
                     </div>
                     <span class="player-name">{{item.name.split('&')[0]}}</span>
                     <span class="player-name">{{item.name.split('&')[1]}}</span>
                     <div class="vote-btn">
                         <span class="votes">{{item.ballot_num}}</span>
-                        <span class="btn">VOTE</span>
+                        <span class="btn" @click="handleViceVote(item)">{{$store.state.lang.mrright_vote}}</span>
                     </div>
                 </li>
             </ul>
         </div>
-        <div class="rank">
+        <div class="rank" v-show="advisorList.length>0">
             <p>
-                <span class="title">History Rank</span>
-                <span class="voteleft">All candidates encore on 14th Jul!</span>
+                <img class="heart" src="~assets/img/vote/heartpoint.png">
+                <span class="title">{{$store.state.lang.mrright_history_rank}}</span>
+                <span class="voteleft">{{$store.state.lang.mrright_all_candidates}}</span>
             </p>
             <ul>
                 <li v-for="(item,index) in showList" :key="index">
                     <div class="left">
-                        <span class="rank-num" :class="{topThree:index<3}">{{index+1}}</span>
-                        <img v-show="index<3" :src="item.icon.replace('http:','https:')"  class="icon">
-                        <span class="name">{{item.name}}</span>
+                        <span class="rank-num" :class="{'top-three':index<3}">{{index+1}}</span>
+                        <img v-show="index<3" :src="item.icon.replace('http:','https:')" class="icon">
+                        <div v-if="index<3" class="name">
+                            <p>{{item.name.split('&')[0]}}</p>
+                            <p> & {{item.name.split('&')[1]}}</p>
+                        </div>
+                        <span v-else class="name">{{item.name.split('&')[0]}} & {{item.name.split('&')[1]}}</span>
                     </div>
-                    <div class="right" :class="{topThree:index<3}">
+                    <div class="right" :class="{'top-three':index<3}">
                         <span class="count">{{item.ballot_num}}</span>
                         <span class="votes">votes</span>
-                        <img src="~assets/img/vote/ic_play_small_white.png" alt="">
+                        <span class="player">
+                            <img src="~assets/img/vote/ic_play_small_white.png" v-show="item.link_vod_code">
+                        </span>
                     </div>
                 </li>
             </ul>
+            <p class="toggleAll" @click="toggleAll">
+                <span v-show="showList.length==6&&rankList.length>6">{{$store.state.lang.mrright_view_all}}</span>
+                <span v-show="showList.length>6">{{$store.state.lang.mrright_view_less}}</span>
+                <img v-show="rankList.length>6" :class="{updown:showAll}" src="~assets/img/vote/view more.png">
+            </p>
         </div>
-        <div class="video">
-            <p>Clips You Can't Miss</p>
+        <div class="clips">
+            <p>
+                <img class="heart" src="~assets/img/vote/heartpoint.png"> {{$store.state.lang.mrright_clips_you_cant_miss}}
+            </p>
+            <ul class="clearfix">
+                <li v-for="(item,index) in clipsList" :key="index">
+                    <div>
+                        <img :src="item.link_url.replace('http:','https:')" class="url">
+                        <img src="~assets/img/vote/ic_play_small_white.png" class="player">
+                    </div>
+                    <span class="title">{{item.description||item.name}}</span>
+                </li>
+            </ul>
+        </div>
+        <div class="title">
+            <div class="video">
+                <img src="~assets/img/vote/full_eps.png">
+                <img src="~assets/img/vote/trailer.png">
+                <img src="~assets/img/vote/highlights.png">
+            </div>
         </div>
     </div>
 </template>
 <script>
+import { downApp } from '~/functions/utils'
 export default {
     layout: 'base',
     data() {
         return {
             isLogin: this.$store.state.user.type || false,
-            vote_id: 4,
+            isApp: this.$store.state.appType,
+            vote_id: 7,
             videoList: [],
             advisorList: [],
             rankList: [],
             voteLeft: '-',
-            showAll:false
+            showAll: false,
+            clipsList: []
         }
     },
     computed: {
@@ -74,23 +108,27 @@ export default {
             } else {
                 return this.rankList.slice(0, 6)
             }
+        },
+        coupleList(){
+            if (this.advisorList.length>0) {
+                return this.advisorList
+            } else {
+                return this.rankList
+            }
         }
     },
     mounted() {
         this.getVoteLeft()
-        this.getVideoList()
         this.getAdvisorList()
+        this.getRankList()
+        this.getVideoList()
     },
     methods: {
         // 获取投票单元数据
         getAdvisorList() {
             this.$axios.get(`/voting/v1/candidates-show?vote_id=${this.vote_id}`).then(res => {
-                if (res.data.code === 0 && res.data.data.length > 0) {
-                    this.advisorList = res.data.data.slice(0, 3)
-                    this.rankList = res.data.data
-                    this.rankList.sort(function(a, b) {
-                        return b.ballot_num - a.ballot_num
-                    })
+                if (res.data.data.length > 0) {
+                    this.advisorList = res.data.data
                 } else {
                 }
             })
@@ -100,7 +138,8 @@ export default {
         getVideoList() {
             this.$axios.get(`/voting/v1/program?vote_id=${this.vote_id}`).then(res => {
                 if (res.data.code === 0) {
-                    this.videoList = res.data.data
+                    this.videoList = res.data.data.slice(0, 3)
+                    this.clipsList = res.data.data.slice(3, 7)
                 }
             })
         },
@@ -109,53 +148,72 @@ export default {
         getVoteLeft() {
             this.$axios.get(`/voting/v1/ballot/user-ballot-nums?vote_id=${this.vote_id}`).then(res => {
                 if (res.data.code === 0) {
-                    this.voteLeft = res.data.data + ''
+                    this.voteLeft = res.data.data
                 }
             })
-        }
+        },
 
         // 获取所有获选人列表，及排行
-        // getRankList() {
-        //     this.$axios.get(`/v1/candidates-with-ballot-num?vote_id=${this.vote_id}`).then(res => {
-        //         if (res.data.code === 0) {
-        //             this.rankList = res.data.data
-        //         }
-        //     })
-        // },
-
+        getRankList() {
+            this.$axios.get(`/voting/v1/candidates-with-ballot-num?vote_id=${this.vote_id}`).then(res => {
+                if (res.data.code === 0) {
+                    this.rankList = res.data.data
+                    this.rankList.sort(function(a, b) {
+                        return b.ballot_num - a.ballot_num
+                    })
+                }
+            })
+        },
         // 投票提交
-        // handleViceVote(advisor) {
-        //     this.sendEvLog({
-        //         category: 'vote_' + this.share.voteName,
-        //         action: 'flowerbtn_click',
-        //         label: 'flower_' + advisor.name,
-        //         value: 10
-        //     })
-        //   if (this.leftflower === 0) {
-        //     this.$alert('flower left: 0 \n Remember to vote tomorrow!')
-        //     return
-        // }
-        // this.$axios({
-        //     url: '/voting/v1/ballot',
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/x-www-form-urlencoded'
-        //     },
-        //     data: qs.stringify({
-        //         candidate_id: advisor.id,
-        //         vote_id: this.tab_msg.vote_id
-        //     })
-        // }).then(res => {
-        //     if (res.data.code === 0) {
-        //         if (this.leftvote === 0) {
-        //             this.$alert('Vote success, vote left: 0 \n Remember to vote tomorrow! Now you can invite friends to vote')
-        //         }
-        //         this.getAdvisorList()
-        //     } else {
-        //         this.$alert(res.data.message)
-        //     }
-        // })
-        // },
+        handleViceVote(advisor) {
+            // 浏览器打开判断用户终端，提示下载
+            if (this.isApp !== 1 && this.isApp !== 2) {
+                const _this = this
+                if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                    console.log(navigator.userAgent)
+                    this.$confirm(
+                        this.$store.state.lang.mrright_download_ios,
+                        () => {
+                            downApp.call(_this)
+                        },
+                        () => {},
+                        this.$store.state.lang.mrright_go,
+                        this.$store.state.lang.mrright_not_now
+                    )
+                } else if (/(Android)/i.test(navigator.userAgent)) {
+                    console.log(navigator.userAgent)
+                    this.$confirm(
+                        this.$store.state.lang.mrright_download_android,
+                        () => {
+                            downApp.call(_this)
+                        },
+                        () => {},
+                        this.$store.state.lang.mrright_go,
+                        this.$store.state.lang.mrright_not_now
+                    )
+                }
+                return
+            }
+            if (this.voteLeft === 0 && this.isLogin) {
+                this.$toast(this.$store.state.lang.mrright_vote_tomorrow_login)
+            } else if (this.voteLeft === 0 && !this.isLogin) {
+                this.$confirm(
+                    this.$store.state.lang.mrright_vote_tomorrow,
+                    () => {
+                        this.$router.push('/hybrid/account/login')
+                    },
+                    () => {},
+                    this.$store.state.lang.mrright_sign_in,
+                    this.$store.state.lang.mrright_no
+                )
+            } else {
+                advisor.ballot_num++
+                this.voteLeft--
+            }
+        },
+        toggleAll() {
+            this.showAll = !this.showAll
+        }
     },
     head() {
         return {
@@ -165,33 +223,18 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+@import '~assets/less/vote/normal.less';
 .Mr-Right {
     width: 100%;
     height: 100%;
+    min-height: 100vh;
     background: #f5a189;
-    .heart{
-        width: .8rem;
-        height: .8rem;
-        display: inline-block;
-        position: relative;
-        margin-right:.2rem;
-    }
-    .heart::before,.heart::after{
-        position: absolute;
-        display: block;
-        content:"";
-        width: .4rem;
-        height: .67rem;
-        background-color: #F93874;
-        border-top-left-radius: 50%;
-        border-top-right-radius: 50%;
-        left: .2rem;
-        top: 0px;
-        transform: rotate(-45deg);
-    }
-    .heart::after{
-        transform: rotate(45deg);
-        left: .4rem;
+    padding-bottom: 2rem;
+    .heart {
+        width: 0.7rem;
+        height: 0.6rem;
+        margin-right: 0.2rem;
+        vertical-align: inherit;
     }
     .title {
         & > img {
@@ -210,6 +253,7 @@ export default {
     }
     .vote {
         padding: 0 2.5%;
+
         p {
             margin: 1rem 0;
             height: 1.25rem;
@@ -227,21 +271,42 @@ export default {
         }
         li {
             list-style: none;
-            width: 32.1%;
+            width: 30%;
             float: left;
-            margin: 0 0.6% 0.5rem;
+            margin: 0 1.5% 0.5rem;
+            &.only-two {
+                margin: 0 10% 0.5rem;
+            }
             .img-box {
+                position: relative;
                 width: 100%;
                 margin-bottom: 0.5rem;
                 overflow: hidden;
                 border-radius: 2px;
                 opacity: 0.9262;
-                border: 2px solid rgba(255, 255, 255, 1);
-                box-shadow: 2px 0px 10px white, 10px 10px 10px white inset;
-                // background: url('~assets/img/vote/pic_outline.png') no-repeat ;
+                border: 2px solid #ffffff;
+                box-shadow: 0px 0px 10px #f7ebe2, 0px 0px 10px #f7ebe2 inset;
+                &:before {
+                    content: '';
+                    display: inline-block;
+                    padding-bottom: 100%;
+                    width: 0.1px;
+                    vertical-align: middle;
+                }
                 .icon {
                     width: 100%;
+                    height: 100%;
                     display: block;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                }
+                .player {
+                    width: 1.5rem;
+                    height: 1.5rem;
+                    position: absolute;
+                    right: 3px;
+                    bottom: 3px;
                 }
             }
             .player-name {
@@ -256,8 +321,8 @@ export default {
             }
             .vote-btn {
                 width: 100%;
-                height: 1.8rem;
-                line-height: 1.4rem;
+                height: 2rem;
+                line-height: 1.6rem;
                 background: #fed56b;
                 border-radius: 15px;
                 font-size: 0.88rem;
@@ -300,6 +365,10 @@ export default {
             position: relative;
             margin: 1rem 0;
             .left {
+                width: 70%;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
                 .rank-num {
                     font-size: 0.88rem;
                     color: #ffffff;
@@ -307,12 +376,17 @@ export default {
                     display: inline-block;
                     width: 2rem;
                     text-align: center;
-                    &.topThree {
+                    &.top-three {
                         color: #ff598c;
                     }
                 }
                 .name {
                     color: #ffffff;
+                    display: inline-block;
+                    vertical-align: middle;
+                    p {
+                        margin: 0.5rem;
+                    }
                 }
                 img {
                     width: 3.75rem;
@@ -325,7 +399,7 @@ export default {
                 right: 0;
                 transform: translateY(-50%);
                 color: #ffffff;
-                &.topThree {
+                &.top-three {
                     color: #f93874;
                 }
                 .count {
@@ -334,15 +408,36 @@ export default {
                 .votes {
                     font-size: 0.88rem;
                 }
-                img {
+                .player {
+                    display: inline-block;
                     width: 1.25rem;
-                    margin-left:0.3rem;
+                    height: 1.25rem;
+                    margin-left: 0.3rem;
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+        .toggleAll {
+            width: 100%;
+            text-align: center;
+            color: #ffffff;
+            font-size: 0.75rem;
+            img {
+                width: 0.6rem;
+                margin-left: 0.2rem;
+                vertical-align: middle;
+                transition: transform 0.3s;
+                &.updown {
+                    transform: rotate(180deg);
                 }
             }
         }
     }
-    .video{
+    .clips {
         padding: 0 2.5%;
+        margin-bottom: 1rem;
         p {
             margin: 1rem 0;
             height: 1rem;
@@ -350,21 +445,51 @@ export default {
             color: #f93874;
             font-weight: bold;
         }
+        li {
+            list-style: none;
+            float: left;
+            width: 48%;
+            line-height: 1.1rem;
+            color: #ffffff;
+            margin-bottom: 0.3rem;
+            &:nth-child(2n) {
+                float: right;
+            }
+            div {
+                position: relative;
+                width: 100%;
+                &:before {
+                    content: '';
+                    display: inline-block;
+                    padding-bottom: 56%;
+                    width: 0.1px;
+                    vertical-align: middle;
+                }
+                .player {
+                    position: absolute;
+                    bottom: 5px;
+                    right: 5px;
+                    width: 1.5rem;
+                }
+                .url {
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                }
+            }
+            span {
+                font-size: 0.88rem;
+                &.title {
+                    display: -webkit-box;
+                    overflow: hidden;
+                    height: 3.6rem;
+                    padding-top: 0.4rem;
+                    -webkit-line-clamp: 3;
+                    /* autoprefixer: off */
+                    -webkit-box-orient: vertical;
+                }
+            }
+        }
     }
-}
-.clearfix:after {
-    display: block;
-    visibility: hidden;
-    clear: both;
-    height: 0;
-    content: '';
-}
-
-.clearfix {
-    zoom: 1;
-}
-
-.clear {
-    clear: both;
 }
 </style>
