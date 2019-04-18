b8a5b4c274c432be2ae0f031f5996ad3281244ce
shangxinbo <shangxb@startimes.com.cn>
2019-04-18 10:29:54 +0800

    动画四号线

--- functions/utils.js
+++ functions/utils.js
@@ -441,14 +441,18 @@ export const playVodinApp = function(appType, vod) {
 
 export const animateCSS = function(element, animationName, callback) {
     const node = element
-    node.classList.add('animated', animationName)
 
     function handleAnimationEnd() {
         node.classList.remove('animated', animationName)
         node.removeEventListener('animationend', handleAnimationEnd)
+        node.removeEventListener('webkitAnimationEnd', handleAnimationEnd)
 
         if (typeof callback === 'function') callback()
     }
 
     node.addEventListener('animationend', handleAnimationEnd)
+    node.addEventListener('webkitAnimationEnd', handleAnimationEnd)
+    node.classList.add('animated', animationName)
+    
+    
 }
