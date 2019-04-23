643df1016055c5b886b131210a88ab1a15eab0d1
naheya <nahy@startimes.com.cn>
2019-04-23 16:41:46 +0800

    404 500 报错页面

---
+++ pages/browser/error404.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="error">
+        <img src="~assets/img/web/error404.png" alt> 404 error
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        this.$nextTick(() => this.$nuxt.$loading.finish())
+    }
+}
+</script>
+<style lang="less" scoped>
+.error {
+    width: 100%;
+    text-align: center;
+    color: #666666;
+    img {
+        display: block;
+        width: 65%;
+        margin: 4rem auto 2rem;
+    }
+}
+</style>
+
+
