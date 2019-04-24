eacb501219fd3cb27ad9124ec01bf5b50f5f9231
shangxinbo <shangxb@startimes.com.cn>
2019-04-24 18:09:41 +0800

    解析日志

--- pages/hybrid/tools/logdecode.vue
+++ pages/hybrid/tools/logdecode.vue
@@ -2,6 +2,22 @@
     <div style="width:1200px;margin:0 auto;padding:20px">
         <textarea v-model="text"/>
         <div class="button" @click="decode">Decode</div>
+        <div v-show="logArr.length>0">
+            <table>
+                <tr>
+                    <th>category</th>
+                    <th>action</th>
+                    <th>label</th>
+                    <th>value</th>
+                </tr>
+                <tr v-for="(item,i) in logArr" :key="i">
+                    <td>{{item.category}}</td>
+                    <td>{{item.action}}</td>
+                    <td>{{item.label}}</td>
+                    <td>{{item.value}}</td>
+                </tr>
+            </table>
+        </div>
         <textarea v-model="formatHTML" class="format" readonly="readonly"/>
     </div>
 </template>
@@ -11,7 +27,8 @@ export default {
     data() {
         return {
             text: '',
-            formatHTML: ''
+            formatHTML: '',
+            logArr: []
         }
     },
     methods: {
@@ -20,12 +37,19 @@ export default {
             const start = decodeText.indexOf('=[') + 1
             const end = decodeText.indexOf(']&') + 1
             const jsonStr = decodeText.substring(start, end).replace(/\+{/g, '{')
+
+            const parseArr = JSON.parse(jsonStr)
+
+            parseArr.forEach(item => {
+                this.logArr.push(item.msg)
+            })
+
             this.formatHTML = JSON.stringify(JSON.parse(jsonStr), null, 4)
         }
     }
 }
 </script>
-<style scoped>
+<style lang="less" scoped>
 textarea {
     width: 100%;
     height: 200px;
@@ -59,5 +83,22 @@ textarea {
 }
 ::-webkit-scrollbar-thumb:window-inactive {
     background: rgba(100, 100, 100, 0.4);
+}
+table {
+    width: 100%;
+    text-align: left;
+    line-height: 3rem;
+    border-left: 1px solid gray;
+    border-top: 1px solid gray;
+    font-size: 14px;
+    margin: 1rem 0;
+    tr {
+        border-bottom: 1px solid gray;
+        td,
+        th {
+            padding: 0 1rem;
+            border-right: 1px solid gray;
+        }
+    }
 }
 </style>
