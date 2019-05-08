c159024c541598ed0d91793f0d81a9e06662cfcc
shangxinbo <shangxb@startimes.com.cn>
2019-05-08 10:01:24 +0800

    是否支持图片

--- functions/utils.js
+++ functions/utils.js
@@ -471,7 +471,7 @@ export const animateCSS = function(element, animationName, callback) {
 export const cdnPicSrc = function(src) {
     if (src) {
         const app = (this.$store && this.$store.state.appType) || 0
-        if (app <= 0 || (typeof Promise !== "undefined" && Promise.toString().indexOf("[native code]") !== -1)) {
+        if (app <= 0 || (window && window.indexedDB)) {
             return src.replace('http:', 'https:')
         } else {
             return src
