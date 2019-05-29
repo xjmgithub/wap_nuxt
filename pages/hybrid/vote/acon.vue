15db971c759b30ffdad023e750bce397a9fa3dc2
shangxinbo <shangxb@startimes.com.cn>
2019-05-29 17:55:16 +0800

    分享多了中括号

--- pages/hybrid/vote/acon.vue
+++ pages/hybrid/vote/acon.vue
@@ -220,8 +220,8 @@ export default {
         share() {
             shareInvite(
                 `${window.location.origin}/hybrid/lands/soccercup?utm_source=startimes_app&utm_medium=activity&utm_campaign=soccercup1`,
-                `【StarTimes ON Cup - Crazy Freekick】Join us as a Country Hero and score for the Team ${this.country.name} to win the cup.`,
-                '',
+                'StarTimes ON Cup - Crazy Freekick',
+                `Join us as a Country Hero and score for the Team ${this.country.name} to win the cup.`,
                 `${window.location.origin}/res_nuxt/img/soccercup.png`
             )
         }
