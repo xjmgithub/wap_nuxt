72f6ffd1da0a19e511542cb07a5cea7d0944ce74
shangxinbo <shangxb@startimes.com.cn>
2019-05-31 15:41:22 +0800

    startba的页面地址

--- pages/hybrid/lands/starba.vue
+++ pages/hybrid/lands/starba.vue
@@ -1,9 +1,5 @@
 <template>
-    <div class="wrapper">
-        <div class="page-top clear">
-            <img class="landing-page-bg-center" src="~assets/img/landpage/landing-page-0903.jpg" alt="StarTimes APP">
-        </div>
-    </div>
+    <div class="wrapper"/>
 </template>
 <script>
 import CallApp from 'callapp-lib'
@@ -51,54 +47,4 @@ export default {
     }
 }
 </script>
-<style>
-html,
-body {
-    background: linear-gradient(to right, #698aad, #2d5078);
-}
-</style>
-<style lang="less" scoped>
-.wrapper {
-    min-height: 100%;
-    background: linear-gradient(to right, #698aad, #2d4f7c);
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    max-width: 1300px;
-    margin: 0 auto;
-    position: relative;
-    padding-top: 0;
-}
-.page-top img {
-    width: 100%;
-    height: 100%;
-    display: block;
-}
-.page-bottom {
-    position: absolute;
-    bottom: 2.5rem;
-    width: 100%;
-}
-.page-bottom .download {
-    width: 65%;
-    margin: 0.1rem auto;
-}
-.page-bottom .download-app-btn {
-    font-size: 1.2rem;
-    line-height: 3rem;
-    height: 3rem;
-    text-align: center;
-    color: rgba(255, 255, 255, 1);
-    background-color: #231f20;
-    border-radius: 0.2rem;
-}
-.page-bottom .download-appstore-wrapper {
-    width: 100%;
-    min-height: 44px;
-    overflow: hidden;
-}
-.page-bottom img {
-    float: left;
-    width: 100%;
-    min-height: 44px;
-}
-</style>
+
