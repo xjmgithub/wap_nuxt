104c8e0469b9bb39171252a9f7700ef2a6fe2595
shangxinbo <shangxb@startimes.com.cn>
2019-06-19 18:45:57 +0800

    loading 不消失问题

--- pages/browser/lands/parter.vue
+++ pages/browser/lands/parter.vue
@@ -1,10 +1,19 @@
 <template>
     <img src="~assets/img/landpage/parter.jpeg">
 </template>
+<script>
+export default {
+    mounted() {
+        this.$nextTick(() => {
+            this.$nuxt.$loading.finish()
+        })
+    }
+}
+</script>
 <style scoped>
-img{
+img {
     display: block;
-    width:100%;
-    padding:1.3rem 0 0;
+    width: 100%;
+    padding: 1.3rem 0 0;
 }
 </style>
