ff322b7afb3060d8a30d08b2729821fab9152c48
shangxinbo <shangxb@startimes.com.cn>
2019-06-21 19:17:13 +0800

    更换斜杠

--- api/film/index.js
+++ api/film/index.js
@@ -7,7 +7,7 @@ export default function(req, res, next) {
     const query = qs.parse(urlobj.search.substr(1))
     if (query.pin && (!req.headers.cookie||req.headers.cookie.indexOf('vote_share_down') < 0)) {
         axios
-            .get(`${env.proxyHost}/voting/v1/token`, {
+            .get(`${env.proxyHost}voting/v1/token`, {
                 headers: {
                     token:
                         'eyJhbGciOiJIUzUxMiJ9.eyJhcHAiOjEsInVpZCI6MTIzNSwiY2NvZGUiOiJUWiIsInJvbGUiOjIsImRldiI6ImFuZG9yaWRfNERBQ0ZBQzYtNUM5OS00NUY0LUI0NzEtQjNDRTY1MEIwRUNDIiwiY3JlYXRlZCI6MTUyNDczNDI4MTAyNiwiZXhwIjoxODQwMDk0MjgxLCJjaWQiOjN9.hl9TS3duu8A0dUKas2UhKZewGEda2K-3fppPRuIgOxsM0F4dB0EqlocifN53Xawqn0AhUgpriwdglZkHxaVzQA'
