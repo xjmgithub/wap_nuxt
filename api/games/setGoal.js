3eeb9de67fb731f79540d394c265a02fb5ebd8b2
shangxinbo <shangxb@startimes.com.cn>
2019-07-08 13:31:32 +0800

    goal

--- api/games/setGoal.js
+++ api/games/setGoal.js
@@ -1,8 +1,4 @@
 // 加球接口
-// token 获取用户信息
-// goals
-// gameId
-// goals 如果goals>5非法请求，如果goals是NaN或小于等于0 非法
 // 查询 当前用户最近一次games_action记录
 // 如果 当前时间减去上一次操作记录时间小于12.5s视为不合法请求
 // 在games_action中插入一条记录
@@ -10,8 +6,9 @@
 import qs from 'qs'
 import axios from 'axios'
 // import redis from 'redis'
-// import { runSql } from '../../functions/mysql.js'
+import { runSql } from '../../functions/mysql.js'
 import { getCookieFromReq } from './func'
+import env from '../../env.js'
 // const client = redis.createClient(6379, 'test1-mysql-redis')
 // client.on('error', function(err) {
 //     console.log('Error ' + err)
@@ -21,6 +18,7 @@ export default function(req, res, next) {
     const urlobj = new URL('http://localhost' + req.url)
     const query = qs.parse(urlobj.search.substr(1))
     const goals = query.goal
+    const gameId = query.gameId || 1
     const _COOKIE = getCookieFromReq(req.headers)
     // client.set('color', 'red', redis.print)
     // client.get('color', function(err, value) {
@@ -29,20 +27,53 @@ export default function(req, res, next) {
     //     client.quit()
     // })
 
+    if (isNaN(goals) || goals > 5 || goals < 1) {
+        res.end(
+            JSON.stringify({
+                code: 101,
+                message: 'the goal is invalid',
+                data: ''
+            })
+        )
+        return false
+    }
+
     axios
-        .get('/cms/users/me', {
+        .get(`${env.apiURL}cms/users/me`, {
             headers: {
                 token: _COOKIE.token || ''
             }
         })
-        .then(res1 => {
+        .then(function(res1) {
             if (res1.status === 200) {
-                console.log(123, goals)
+                const userId = res.data.data.id
+                runSql(
+                    res,
+                    `SELECT create_time FROM games_action WHERE user_id="${userId}" AND fk_game="${gameId}" AND action_name="goals" ORDER BY create_time limit 1`,
+                    (error, lastTime) => {
+                        if (error) {
+                            res.end(
+                                JSON.stringify({
+                                    code: 104,
+                                    message: 'search error',
+                                    data: error
+                                })
+                            )
+                        } else if (lastTime) {
+                            console.log(lastTime)
+                        } else {
+                            console.log(123)
+                        }
+                    }
+                )
             } else {
-                res.send(401)
+                res.statusCode = 401
+                res.end('Unauthorized')
             }
         })
-        .catch(() => {
-            res.end(401)
+        .catch(err => {
+            console.log(err)
+            res.statusCode = 401
+            res.end('Unauthorized')
         })
 }
