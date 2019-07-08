dbfde9eed56355a4b7c0f06c3105c965a3ff7383
naheya <nahy@startimes.com.cn>
2019-07-08 09:28:12 +0800

    爱的宣言活动结束，当前状态只显示排行榜

--- pages/hybrid/vote/LoveShooter.vue
+++ pages/hybrid/vote/LoveShooter.vue
@@ -7,25 +7,18 @@
                     <span>{{item.name}}</span>
                 </a>
             </nav>
-            <div v-if="tabIndex!=2" id="leftvote" :class="{'line':tabIndex==1 }" class="clearfix">
-                <span>Votes left:{{leftVote}}</span>
-            </div>
             <template v-for="(item,index) in tabList">
-                <mVote v-if="item.type=='vote'" v-show="tabIndex==index" :key="index" :data-list="playerList" @onVote="handleVote" @toPlay="toVideo" />
                 <mRank v-if="item.type=='rank'" v-show="tabIndex==index" :key="index" :data-list="rankList" />
             </template>
         </div>
     </div>
 </template>
 <script>
-import mVote from '~/components/vote/vote'
 import mRank from '~/components/vote/rank'
 import mVoteSwiper from '~/components/vote/vote_swiper'
-import qs from 'qs'
 export default {
     layout: 'base',
     components: {
-        mVote,
         mRank,
         mVoteSwiper
     },
@@ -34,18 +27,11 @@ export default {
             appType: this.$store.state.appType || 0,
             tabList: [
                 {
-                    name: 'Vote',
-                    type: 'vote'
-                },
-
-                {
                     name: 'Rank',
                     type: 'rank'
                 }
             ],
             tabIndex: 0,
-            leftVote: '-',
-            playerList: [],
             rankList: [],
             voteTitle: 'Love Shooter'
         }
@@ -67,21 +53,12 @@ export default {
     },
     mounted() {
         this.getAllList()
-        this.getvoteLeft()
     },
     methods: {
-        getvoteLeft() {
-            this.$axios.get(`/voting/v1/ballot/user-ballot-nums?vote_id=9`).then(res => {
-                if (res.data.code === 0) {
-                    this.leftVote = res.data.data
-                }
-            })
-        },
         getAllList() {
             this.$axios.get(`/voting/v1/candidates-show?vote_id=9`).then(res => {
                 if (res.data.data.length > 0) {
                     const data = res.data.data
-                    this.playerList = data.concat([])
                     this.rankList = data.concat([])
                     this.rankList.sort(function(a, b) {
                         return b.ballot_num - a.ballot_num
@@ -89,41 +66,6 @@ export default {
                 }
             })
         },
-        handleVote(item) {
-            if (this.leftVote <= 0) {
-                this.$toast('Left vote not enough')
-            } else {
-                this.$axios({
-                    url: '/voting/v1/ballot',
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    data: qs.stringify({
-                        candidate_id: item.id,
-                        vote_id: 9
-                    })
-                }).then(res => {
-                    
-                    if (res.data.code === 0) {
-                        // this.getAllList()
-                        
-                        this.leftVote--
-                        this.$toast('Vote success' + ':' + this.leftVote)
-                    } else {
-                        this.$toast(res.data.message)
-                    }
-                })
-            }
-        },
-        toVideo(item) {
-            const vod = item.link_vod_code
-            if (this.appType == 1 && vod) {
-                window.getChannelId && window.getChannelId.toAppPage(3, 'com.star.mobile.video.player.PlayerVodActivity?vodId=' + vod, '')
-            } else if (this.appType == 2 && vod) {
-                window.location.href = 'startimes://player?vodId=' + vod
-            }
-        }
     },
     head() {
         return {
@@ -140,4 +82,7 @@ export default {
 @navActiveColor: #ffffff;
 @invitedColor: #0087eb;
 @voteleftColor: #fd60a1;
+#nav a{
+    width: 100%;
+}
 </style>
