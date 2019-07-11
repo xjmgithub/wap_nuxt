2da9d92fe0850716cd1d4763ff2444e0c02591ae
shangxinbo <shangxb@startimes.com.cn>
2019-07-11 20:18:41 +0800

    countly日志

--- pages/hybrid/faq/customerService.vue
+++ pages/hybrid/faq/customerService.vue
@@ -348,19 +348,19 @@ export default {
                     return false
                 }
                 
-                this.sendEvLog({
-                    category: 'onlineService',
-                    action: 'error_debug',
-                    label: 1,
-                    value: JSON.stringify({
-                        service_type: this.connectState == 2 ? 3 : 1, // TODO
-                        service_group_id: this.serviceRecord || '',
-                        service_state: 2,
-                        remark: JSON.stringify(obj),
-                        service_info: serviceInfo,
-                        operator: operator
-                    })
-                })
+                // this.sendEvLog({
+                //     category: 'onlineService',
+                //     action: 'error_debug',
+                //     label: 1,
+                //     value: JSON.stringify({
+                //         service_type: this.connectState == 2 ? 3 : 1, // TODO
+                //         service_group_id: this.serviceRecord || '',
+                //         service_state: 2,
+                //         remark: JSON.stringify(obj),
+                //         service_info: serviceInfo,
+                //         operator: operator
+                //     })
+                // })
 
                 this.$axios
                     .post('/css/v1/service/history', {
