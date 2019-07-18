25726c00b2dc13ebc220a4e7926545bab509eb0b
shangxinbo <shangxb@startimes.com.cn>
2019-07-18 17:38:06 +0800

    getword

--- api/games/func.js
+++ api/games/func.js
@@ -1,3 +1,7 @@
+import axios from 'axios'
+import env from '../../env.js'
+import qs from 'qs'
+
 export const getCookieFromReq = function(headers) {
     if (headers && headers.cookie) {
         const _COOKIE = {}
@@ -9,4 +13,64 @@ export const getCookieFromReq = function(headers) {
     } else {
         return {}
     }
+}
+
+export const getUserMe = function(token, callback) {
+    return axios
+        .get(`${env.apiURL}cms/users/me`, {
+            headers: {
+                token: token || ''
+            }
+        })
+        .then(res => {
+            const userId = res.data.id
+            const countryId = res.data.areaID
+            const userAvatar = res.data.head
+            callback && callback(userId, countryId, userAvatar)
+        })
+}
+
+export const addCoins = function(token, userId, coins, des, callback) {
+    if (!token || !userId || isNaN(coins) || coins <= 0 || !des) {
+        callback && callback()
+        return
+    }
+
+    axios({
+        url: `/promotion-service/coins/add`,
+        method: 'post',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            token: token
+        },
+        data: qs.stringify({
+            userId: userId,
+            coins: coins,
+            description: des
+        })
+    }).then(res => {
+        callback && callback(res)
+    })
+}
+export const delCoins = function(token, userId, coins, des, callback) {
+    if (!token || !userId || isNaN(coins) || coins <= 0 || !des) {
+        callback && callback()
+        return
+    }
+
+    axios({
+        url: `/promotion-service/coins/sub`,
+        method: 'post',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            token: token
+        },
+        data: qs.stringify({
+            userId: userId,
+            coins: coins,
+            description: des
+        })
+    }).then(res => {
+        callback && callback(res)
+    })
 }
--- api/games/getAward.js
+++ api/games/getAward.js
@@ -2,19 +2,18 @@
 
 // headers token
 // goals
-// gameId 
+// gameId
 
 // 调取用户信息接口，获得用户信息，校验合法性
 // 获取当前用户当天获得的coins列表 type==1 coins_log
 // 求列表总和，求最近一次的时间
 // 如果当前时间减最后一次时间的diff < goals*2 *1000,不合法请求
 // 第一关限制1个，第2，3关2个，4关后3个
-// 进60球的最小时间是(Math.ceil((60-1-2*2)/3) + 3) *5 * 2.5 = 275 
+// 进60球的最小时间是(Math.ceil((60-1-2*2)/3) + 3) *5 * 2.5 = 275
 // 进30球的   (Math.ceil((30-1-2*2)/3) + 3) *5 * 2.5 = 150
 // 进10球的   (Math.ceil((10-1-2*2)/3) + 3) *5 * 2.5 = 62.5
 
-
-// 计算应该加的coins 
+// 计算应该加的coins
 // >60 800
 // >30 400
 // >10 100
@@ -22,9 +21,92 @@
 // addCoins + sum > 30000     则将奖励重置成 30000 - sum
 
 // 做加操作 可公共出来
-// 创建一条coins_log  state = 0  记录id
-// 发起加操作的请求 之后更新记录id
+// 发起加操作的请求 之后创建coins_log 记录
 
-// 获取最新coins 返回给前端
+// 返回给前端的当前的coins
 
-  
+import qs from 'qs'
+import dayjs from 'dayjs'
+import { runSql } from '../../functions/mysql.js'
+import { getUserMe, addCoins } from './func'
+
+export default function(req, res, next) {
+    const urlobj = new URL('http://localhost' + req.url)
+    const query = qs.parse(urlobj.search.substr(1))
+    const goals = query.goals
+    const gameId = query.gameId || 1
+    const token = req.headers.token
+    const now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    const today = dayjs().format('YYY-MM-DD')
+    const tomorrow = dayjs()
+    let award = (0).add(1, 'days').format('YYY-MM-DD')
+
+    if (isNaN(goals) || goals > 100 || goals < 1) {
+        res.end(
+            JSON.stringify({
+                code: 101,
+                message: 'the goal is invalid',
+                data: ''
+            })
+        )
+        return false
+    } else if (goals > 10) {
+        award = 100
+    } else if (goals > 30) {
+        award = 400
+    } else if (goals > 60) {
+        award = 800
+    }
+
+    getUserMe(token, (user, countryid) => {
+        runSql(
+            res,
+            `SELECT id,coins,create_time FROM coins_log WHERE user_id=${user} AND TYPE=1 AND state!=2 AND fk_game=${gameId} AND create_time>'${today}' AND create_time<'${tomorrow}'`,
+            result => {
+                if (result.length > 0) {
+                    // shang
+                }
+
+                addCoins(token, user, award, `Shot Games-${goals} goals`, addResult => {
+                    const resText = JSON.stringify(addResult.data).substr(0, 800)
+                    if (addResult && addResult.data.code == 0) {
+                        const coinsActionid = addResult.data.id
+                        const resText = JSON.stringify(addResult.data).substr(0, 800)
+                        runSql(
+                            res,
+                            `INSERT INTO coins_log (type,coins,user_id,instruction,state,fk_game,coins_action_id,res_text,create_time) VALUES ('1',${award},${user},'Shot Games-${goals} goals',1,${gameId},${coinsActionid},${resText},'${now}')`,
+                            () => {
+                                res.end(
+                                    JSON.stringify({
+                                        code: 200,
+                                        message: 'success',
+                                        data: {
+                                            operatorCoins: addResult.data.operatorCoins,
+                                            beforeCoins: addResult.data.beforeCoins,
+                                            afterCoins: addResult.data.afterCoins
+                                        }
+                                    })
+                                )
+                            }
+                        )
+                    } else {
+                        // 兑换积分失败
+                        runSql(
+                            res,
+                            `INSERT INTO coins_log (type,coins,user_id,instruction,state,fk_game,coins_action_id,res_text,create_time) VALUES ('1',${award},${user},'Shot Games-${goals} goals',1,${gameId},'',${resText},'${now}')`,
+                            () => {
+                                res.end(
+                                    JSON.stringify({
+                                        code: 102,
+                                        message: 'get coins error',
+                                        data: ''
+                                    })
+                                )
+                            }
+                        )
+                    }
+                })
+            }
+        )
+    })
+}
