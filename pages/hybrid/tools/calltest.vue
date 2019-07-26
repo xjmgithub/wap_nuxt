9e022ae2df465fa41a9e0b8ecc74f098d889b955
shangxinbo <shangxb@startimes.com.cn>
2019-07-26 19:36:43 +0800

    测试唤醒方案

---
+++ pages/hybrid/tools/calltest.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="calltest">
+        <div class="blue" @click="intent">intent唤醒</div>
+        <div class="green" @click="scheme">普通scheme唤醒</div>
+        <div class="ua">{{ua}}</div>
+    </div>
+</template>
+<script>
+import { envokeByIntent, invokeByIframe } from '~/functions/app'
+export default {
+    layout: 'base',
+    data() {
+        return {
+            ua: ''
+        }
+    },
+    mounted() {
+        this.ua = window.navigator.userAgent
+    },
+    methods: {
+        intent() {
+            envokeByIntent.call(this,'com.star.mobile.video.player.PlayerVodActivity?vodId=234')
+        },
+        scheme() {
+            invokeByIframe.call(this)
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.calltest {
+    div {
+        width: 100%;
+        height: 5rem;
+        text-align: center;
+        line-height: 5rem;
+    }
+    .blue {
+        background: blue;
+    }
+    .green {
+        background: green;
+    }
+    .ua {
+        line-height: 1.5rem;
+        text-align: left;
+    }
+}
+</style>
+
---
+++ pages/hybrid/tools/ua.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{ua}}</div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    data() {
+        return {
+            ua: ''
+        }
+    },
+    mounted() {
+        this.ua = window.navigator.userAgent
+    }
+}
+</script>
