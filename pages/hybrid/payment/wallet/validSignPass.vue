998bea17d0ecb097e8ab6e07183da6e4485c1698
shangxinbo <shangxb@startimes.com.cn>
2019-08-13 19:32:40 +0800

    添加验证登录密码

--- pages/hybrid/payment/wallet/validSignPass.vue
+++ pages/hybrid/payment/wallet/validSignPass.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container">
-        <div class="input-item">
+        <div class="input-item" :class="{error:error_password}">
             <div class="label">
                 Enter sign in password
                 <img v-show="isCiphertext==1" class="open-close" src="~assets/img/ic_hide_def_g.png" @click="isCiphertext=2" />
                 <img v-show="isCiphertext==2" class="open-close" src="~assets/img/ic_show_def_g.png" @click="isCiphertext=1" />
             </div>
             <input v-model="password" :type="pwdType" />
+            <div class="error-text">{{error_password}}</div>
         </div>
         <div class="footer">
             <mButton :disabled="password.length<6" text="NEXT" @click="checkSignPass" />
@@ -15,6 +16,8 @@
 </template>
 <script>
 import mButton from '~/components/button'
+import countrys from '~/functions/countrys.json'
+import { setCookie } from '~/functions/utils'
 export default {
     layout: 'base',
     components: {
@@ -23,7 +26,8 @@ export default {
     data() {
         return {
             password: '',
-            isCiphertext: 1
+            isCiphertext: 1,
+            error_password: ''
         }
     },
     computed: {
@@ -31,16 +35,65 @@ export default {
             return this.isCiphertext === 1 ? 'password' : 'text'
         }
     },
+    watch: {
+        password(nv, ov) {
+            this.error_password = ''
+        }
+    },
     methods: {
         checkSignPass(num) {
             // TODO check sigin
-            this.$router.push({
-                path: '/hybrid/payment/wallet/setPassword',
-                query: {
-                    channel: this.$route.query.channel,
-                    payToken: this.$route.query.payToken
+            const user = this.$store.state.user
+            let opt = {}
+            // 处理用户信息
+            if (user.type == 'Tenbre') {
+                opt = {
+                    applicationId: 2,
+                    deviceId: this.$store.state.deviceId,
+                    type: '0',
+                    email: user.userName,
+                    pwd: this.password
                 }
-            })
+            } else {
+                let pre = ''
+                let phone = ''
+                countrys.forEach(item => {
+                    if (item.phonePrefix && user.userName.indexOf(item.phonePrefix) == 0) {
+                        pre = item.phonePrefix
+                        phone = user.userName.substr(item.phonePrefix.length)
+                    }
+                })
+
+                opt = {
+                    applicationId: 2,
+                    phoneCc: pre,
+                    phone: phone,
+                    pwd: this.password,
+                    deviceId: this.$store.state.deviceId,
+                    type: 10
+                }
+            }
+
+            this.$axios
+                .post('/ums/v3/user/login', opt)
+                .then(res => {
+                    if (res.data.code == 0) {
+                        this.$store.commit('SET_TOKEN', res.data.data.token)
+                        setCookie('token', res.data.data.token)
+                        this.$router.push({
+                            path: '/hybrid/payment/wallet/setPassword',
+                            query: {
+                                channel: this.$route.query.channel,
+                                payToken: this.$route.query.payToken
+                            }
+                        })
+                    } else {
+                        this.error_password = 'Password error'
+                    }
+                })
+                .catch(() => {
+                    this.error_password = 'Password error'
+                })
         }
     }
 }
@@ -56,6 +109,7 @@ export default {
         height: 2rem;
         line-height: 2rem;
     }
+
     .input-item {
         margin-top: 2rem;
     }
@@ -63,6 +117,15 @@ export default {
         width: 1.5rem;
         height: 1.5rem;
         float: right;
+    }
+    .error {
+        input {
+            border-bottom: red solid 1px;
+        }
+        .error-text {
+            color: red;
+            line-height: 1.5rem;
+        }
     }
 }
 .footer {
