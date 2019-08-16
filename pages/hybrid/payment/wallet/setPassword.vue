d9dae379784025fe80d88c574fc440b13adb61a5
naheya <nahy@startimes.com.cn>
2019-08-16 17:28:58 +0800

    ss

--- pages/hybrid/payment/wallet/setPassword.vue
+++ pages/hybrid/payment/wallet/setPassword.vue
@@ -7,7 +7,7 @@
                 :disabled="reset"
                 @canNext="canStep2=true"
             />
-            <div class="change-phone" v-if="!passIsSet">
+            <div v-if="!passIsSet" class="change-phone">
                 <nuxt-link to="/hybrid/payment/wallet/resetPhone">Change cellphone number</nuxt-link>
             </div>
             <div class="footer">
