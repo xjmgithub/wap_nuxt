6d3fefd01e599fade893f37d790be67e7dc164e4
shangxinbo <shangxb@startimes.com.cn>
2019-08-19 11:52:54 +0800

    ios 唤醒节目

--- pages/hybrid/tools/applinks.vue
+++ pages/hybrid/tools/applinks.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <a :href="link">点击</a>
+        <a :href="ioslink">点击scheme唤醒</a>
     </div>
 </template>
 <script>
+import { Base64 } from 'js-base64'
 export default {
     layout: 'base',
     data() {
         return {
-            link: this.$route.query.link
+            link: this.$route.query.link,
+            ioslink: 'startimes://?target=' + Base64.encode('startimes://player?vodId=59072')
         }
     }
 }
