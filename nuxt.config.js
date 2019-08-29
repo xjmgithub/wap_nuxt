6d76218e83c55e8228c1a4e1c615fa9ebd75b173
shangxinbo <shangxb@startimes.com.cn>
2019-08-29 09:18:49 +0800

    remove qatest

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,8 +66,7 @@ export default {
         '/search-service/': env.proxyHost,
         '/wxorder/': env.proxyHost,
         '/like/': env.proxyHost,
-        '/feed/': env.proxyHost,
-        '/payment/api/v2/': `http://qa.payment.startimestv.com/payment/api/v2/`
+        '/feed/': env.proxyHost
     },
     router: {
         middleware: 'auth' // middleware before all pages render
