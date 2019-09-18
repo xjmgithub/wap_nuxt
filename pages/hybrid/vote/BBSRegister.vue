528f7b6ea4151d2099f77df7d93bce9503b79357
Tiwen007 <18811716915@163.com>
2019-09-18 14:59:45 +0800

    save banner

--- pages/hybrid/vote/BBSRegister.vue
+++ pages/hybrid/vote/BBSRegister.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="wrapper">
+        <img src="~assets/img/vote/BSSRegister/bg-img.jpg" alt="bg-img" />
+        <div class="tab-box">
+            <div class="tab">
+                <div class="rules" @click="show_rules=true">
+                    <p>RULES</p>
+                    <img src="~assets/img/vote/BSSRegister/ic-rule.png" alt="ic-rule" />
+                </div>
+                <div class="share" @click="toShare('upshare')">
+                    <p>SHARE</p>
+                    <img src="~assets/img/vote/BSSRegister/ic-share.png" alt="ic-share" />
+                </div>
+            </div>
+        </div>
         <div class="registration">
             <h4>Registration</h4>
             <div>
@@ -26,7 +39,7 @@
                     Birthday
                     <sup class="required">*</sup>
                 </span>
-                <select v-model="formData.year" class="birth" @change="getMouth" >
+                <select v-model="formData.year" class="birth" @change="getMouth">
                     <option value disabled selected class="normal">year</option>
                     <option v-for="item in year" :key="item" :value="item">{{item}}</option>
                 </select>
@@ -34,7 +47,7 @@
                     <option value disabled selected class="normal">mouth</option>
                     <option v-for="item in month" :key="item" :value="item">{{item}}</option>
                 </select>
-                <select v-model="formData.day" class="birth" >
+                <select v-model="formData.day" class="birth">
                     <option value disabled selected class="normal">day</option>
                     <option v-for="item in day" :key="item" :value="item">{{item}}</option>
                 </select>
@@ -69,10 +82,22 @@
             <p v-show="validNum==false&&number.length" class="error">Please enter a valid phone number</p>
             <div class="disabled" :class="{'submit':canSubmit&&validNum}" @click="submit()">SUBMIT</div>
         </div>
+        <div class="past-programme">
+            <ul class="clearfix">
+                <li v-for="(item,index) in clipsList" :key="index">
+                    <div @click="toPlayer(item.link_vod_code,'video_click',item.name||item.description,10)">
+                        <img class="url" :src="cdnPic(item.cover)" />
+                        <img src="~assets/img/vote/ic_play_small_white.png" class="player" />
+                    </div>
+                    <span class="title">{{item.description||item.name}}</span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
 import qs from 'qs'
+import { cdnPicSrc } from '~/functions/utils'
 export default {
     layout: 'base',
     components: {},
@@ -114,7 +139,11 @@ export default {
             validNum: null,
             birthdayErr: false,
             link: '',
-            repeatSub: true
+            repeatSub: true,
+            enroll_id: 1,
+            vote_id: 14,
+            clipsList: [],
+            loaded: false
         }
     },
     computed: {
@@ -128,12 +157,42 @@ export default {
             return true
         }
     },
-    watch: {},
+    asyncData({ app: { $axios }, store }) {
+        return {
+            serverTime: new Date()
+        }
+    },
+    created() {
+        this.getRegisterInfo()
+    },
     mounted() {
         this.getYear()
-        // this.getMouth()
+        this.getVideoMsg()
     },
     methods: {
+        cdnPic(src) {
+            return cdnPicSrc.call(this, src)
+        },
+        // 获取报名活动信息
+        getRegisterInfo() {
+            this.currentTime = Date.parse(this.serverTime)
+            this.$axios
+                .get(`/voting/enroll/v1/info?enroll_id=${this.enroll_id}`)
+                .then(res => {
+                    if (res.data.code === 200) {
+                        this.startTime = new Date(res.data.data.start_time).getTime()
+                        this.endTime = new Date(res.data.data.end_time).getTime()
+                        if (this.currentTime < this.startTime) {
+                            this.$alert('活动未开始', () => {}, 'GOT IT')
+                        } else if (this.currentTime >= this.endTime) {
+                            this.$alert('活动已结束', () => {}, 'GOT IT')
+                        }
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
         // 获取生日年份
         getYear() {
             for (let i = 0; i < 100; i++) {
@@ -168,11 +227,16 @@ export default {
         },
         // 提交表单
         submit() {
-            if(typeof(this.formData.year)=='string' || typeof(this.formData.month)=='string' || typeof(this.formData.day)=='string') {
+            if (typeof this.formData.year == 'string' || typeof this.formData.month == 'string' || typeof this.formData.day == 'string') {
                 this.birthdayErr = true
                 return
-            } 
-            const birthday = this.formData.year + '-' + (this.formData.month.toString().length <= 1 ? '0' + this.formData.month : this.formData.month) + '-' +(this.formData.day.toString().length <= 1 ? '0' + this.formData.day : this.formData.day)
+            }
+            const birthday =
+                this.formData.year +
+                '-' +
+                (this.formData.month.toString().length <= 1 ? '0' + this.formData.month : this.formData.month) +
+                '-' +
+                (this.formData.day.toString().length <= 1 ? '0' + this.formData.day : this.formData.day)
             console.log(birthday)
 
             if (new Date() < new Date(birthday)) {
@@ -189,14 +253,14 @@ export default {
                         'Content-Type': 'application/x-www-form-urlencoded'
                     },
                     data: qs.stringify({
-                        "fk_enroll": 1,
-                        "name": this.name,
-                        "gender": this.gender,
-                        "birthday": this.birthday,
-                        "phone": this.number,
-                        "address": this.city,
-                        "profession": this.profession,
-                        "other_description": this.link,
+                        fk_enroll: 1,
+                        name: this.name,
+                        gender: this.gender,
+                        birthday: this.birthday,
+                        phone: this.number,
+                        address: this.city,
+                        profession: this.profession,
+                        other_description: this.link
                     })
                 })
                     .then(res => {
@@ -208,14 +272,26 @@ export default {
                         }
                         this.repeatSub = true
                     })
-                    .catch((err) => {
+                    .catch(err => {
                         console.log(err)
                     })
             } else {
                 console.log('false')
             }
+        },
+        getVideoMsg() {
+            // 获取投票单元数据
+            this.$axios.get(`/voting/v1/program?vote_id=${this.vote_id}`).then(res => {
+                if (res.data.code === 0) {
+                    this.clipsList = res.data.data
+                    // this.clipsList.forEach(ele => {
+                    //     this.mSendEvLog('video_show', ele.name || ele.description, 10)
+                    // })
+                }
+            })
         }
     },
+
     head() {
         return {
             title: 'Bongo Star Search'
@@ -226,6 +302,56 @@ export default {
 <style lang="less" scoped>
 @import '~assets/less/vote/normal.less';
 .wrapper {
+    > img {
+        width: 100%;
+        height: auto;
+    }
+    .tab-box {
+        width: 100%;
+        height: auto;
+        position: relative;
+        .tab {
+            width: 100%;
+            position: absolute;
+            color: #fff;
+            top: -3.6rem;
+            left: 0;
+            height: 3rem;
+            line-height: 3rem;
+            .rules {
+                width: 6rem;
+                float: left;
+                background: url('~assets/img/vote/BSSRegister/btn-rules.png') no-repeat;
+                background-size: 6rem 3rem;
+                text-align: left;
+                padding-left: 0.5rem;
+                position: relative;
+                img {
+                    width: 1rem;
+                    height: auto;
+                    position: absolute;
+                    left: 3.8rem;
+                    top: 0.9rem;
+                }
+            }
+            .share {
+                width: 6rem;
+                float: right;
+                background: url('~assets/img/vote/BSSRegister/btn-share.png') no-repeat;
+                background-size: 6rem 3rem;
+                text-align: right;
+                padding-right: 0.5rem;
+                position: relative;
+            }
+            img {
+                width: 1rem;
+                height: auto;
+                position: absolute;
+                right: 3.8rem;
+                top: 1.2rem;
+            }
+        }
+    }
     .registration {
         padding: 1.5rem 5%;
         color: #999999;
@@ -312,6 +438,62 @@ export default {
             font-size: 0.95rem;
             &.submit {
                 background: #00cccc;
+            }
+        }
+    }
+    .past-programme {
+        padding: 0 2.5%;
+        margin-bottom: 1rem;
+        p {
+            margin: 1rem 0;
+            height: 1rem;
+            line-height: 1rem;
+            color: #f93874;
+            font-weight: bold;
+        }
+        li {
+            list-style: none;
+            float: left;
+            width: 48%;
+            line-height: 1.1rem;
+            color: #000;
+            margin-bottom: 0.3rem;
+            &:nth-child(2n) {
+                float: right;
+            }
+            div {
+                position: relative;
+                width: 100%;
+                &:before {
+                    content: '';
+                    display: inline-block;
+                    padding-bottom: 56%;
+                    width: 0.1px;
+                    vertical-align: middle;
+                }
+                .player {
+                    position: absolute;
+                    bottom: 5px;
+                    right: 5px;
+                    width: 1.5rem;
+                }
+                .url {
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                }
+            }
+            span {
+                font-size: 0.88rem;
+                &.title {
+                    display: -webkit-box;
+                    overflow: hidden;
+                    height: 3.6rem;
+                    padding-top: 0.4rem;
+                    -webkit-line-clamp: 3;
+                    /* autoprefixer: off */
+                    -webkit-box-orient: vertical;
+                }
             }
         }
     }
