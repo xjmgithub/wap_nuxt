6c83525b7c687ece174682019deea99c4bdda11c
shangxinbo <shangxb@startimes.com.cn>
2019-09-27 16:32:02 +0800

    唤醒on

--- pages/hybrid/tools/batest.vue
+++ pages/hybrid/tools/batest.vue
@@ -1,5 +1,8 @@
 <template>
-    <div style="width:100%;height:10rem;text-align:center;line-height:10rem;" @click="callup">点击唤醒app</div>
+    <div>
+        <div style="width:100%;height:10rem;text-align:center;line-height:10rem;" @click="callup">点击唤醒ba</div>
+        <div style="width:100%;height:10rem;text-align:center;line-height:10rem;" @click="callOn">点击唤醒ON</div>
+    </div>
 </template>
 <script>
 export default {
@@ -7,6 +10,9 @@ export default {
     methods: {
         callup() {
             window.location.href = 'starba://platformapi/webtoapp'
+        },
+        callOn() {
+            window.location.href = 'starvideo://platformapi/webtoapp'
         }
     }
 }
