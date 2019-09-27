dc0ed186d0cb2f0a8d189fc6732edcb9d84e3702
shangxinbo <shangxb@startimes.com.cn>
2019-09-27 15:33:33 +0800

    唤醒app

---
+++ pages/hybrid/tools/batest.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="width:100%;height:10rem;text-align:center;line-height:10rem;" @click="callup">点击唤醒app</div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    methods: {
+        callup() {
+            window.location.href = 'starba://platformapi/webtoapp'
+        }
+    }
+}
+</script>
