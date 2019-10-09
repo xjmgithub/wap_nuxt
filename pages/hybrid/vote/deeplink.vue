dc840dd3697e6c4c5cc4d133a72da0ee2c5f6630
naheya <nahy@startimes.com.cn>
2019-10-09 18:05:02 +0800

    deeplink页面基本布局

---
+++ pages/hybrid/vote/deeplink.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="wrapper">
+        <img class="st_logo" src="~assets/img/ic_upgrade_icon.png" />
+        <p class="redeem">
+            Redeem your StarTimes voucher
+        </p>
+        <p class="desc">
+            Please enter your voucher code to redeem the voucher into your StarTimes account.
+        </p>
+        <div class="input">
+            <input type="text" v-model="code">
+        </div>
+        <div class="btn">
+            REDEEM VOUCHER
+        </div>
+        <div class="sign-out">
+            Sign Out
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    data() {
+        return {
+            pre: this.$route.query.pre,
+            code: 'V123456789012345'
+        }
+    },
+    mounted() {
+        if (this.pre) {
+            sessionStorage.setItem('login_prefer', this.pre)
+        }
+    },
+    methods: {}
+}
+</script>
+<style lang="less" scoped>
+.wrapper {
+    padding: 3.5rem 1rem 1rem;
+    text-align: center;
+    .st_logo {
+        width: 65%;
+    }
+    .redeem {
+        color: #333333;
+        font-size: 1.2rem;
+        font-weight: bold;
+        margin: 2rem auto;
+        width: 100%;
+    }
+    .desc {
+        color: #666666;
+        font-size: 0.8rem;
+        width: 85%;
+        margin: 0 auto;
+    }
+    .input {
+        width: 85%;
+        margin: 2rem auto;
+        border: 1px solid #999999;
+        border-radius: 2px;
+        input {
+            width: 100%;
+            color: #666666;
+            font-size: 1.2rem;
+            font-weight: bold;
+            text-align: center;
+            background: #eeeeee;
+            padding: 0.5rem 0;
+            outline: none;
+            border: none;
+        }
+    }
+    .btn {
+        width: 85%;
+        margin: 2rem auto;
+        text-align: center;
+        color: #ffffff;
+        font-weight: bold;
+        background: #0087eb;
+        padding: 0.7rem 0;
+    }
+    .sign-out {
+        position: fixed;
+        bottom: 1rem;
+        width: 50%;
+        text-align: center;
+        color: #0087eb;
+        text-decoration: underline;
+        font-size: 0.9rem;
+        left: 50%;
+        margin-left: -25%;
+    }
+}
+</style>
---
+++ pages/hybrid/vote/deeplinkResult.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="wrapper">
+        <div class="success" v-show="result==1">
+            <p class="redeem">
+                Redeem Successful!
+            </p>
+            <p class="desc">
+                You have successfully redeemed your voucher, you can now check your voucher in StarTimes ON app and start watching great contents!
+            </p>
+            <img class="st_logo" src="~assets/img/faq/ic_happy_sl_green.png" />
+            <div class="btn">
+                DOWNLOAD NOW
+            </div>
+        </div>
+        <div class="fail" v-show="result==2">
+            <p class="redeem">
+                Redeem Failed!
+            </p>
+            <p class="desc">
+                You have successfully redeemed your voucher, you can now check your voucher in StarTimes ON app and start watching great contents!
+            </p>
+            <img class="st_logo" src="~assets/img/faq/ic_disappoint_sl_red.png" />
+            <div class="btn">
+                RETRY
+            </div>
+        </div>
+        <div class="sign-out">
+            Sign Out
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    data() {
+        return {
+            result: 2
+        }
+    },
+    methods: {}
+}
+</script>
+<style lang="less" scoped>
+.wrapper {
+    padding: 4rem 1rem 1rem;
+    text-align: center;
+    .redeem {
+        color: #333333;
+        font-size: 1.25rem;
+        font-weight: bold;
+        width: 100%;
+    }
+    .desc {
+        color: #666666;
+        font-size: 0.9rem;
+        width: 85%;
+        margin: 2rem auto;
+    }
+    .st_logo {
+        width: 6rem;
+    }
+    .btn {
+        width: 85%;
+        margin: 2rem auto;
+        text-align: center;
+        color: #ffffff;
+        font-weight: bold;
+        background: #0087eb;
+        padding: 0.7rem 0;
+    }
+    .sign-out {
+        position: fixed;
+        bottom: 1rem;
+        width: 50%;
+        text-align: center;
+        color: #0087eb;
+        text-decoration: underline;
+        font-size: 0.9rem;
+        left: 50%;
+        margin-left: -25%;
+    }
+}
+</style>
