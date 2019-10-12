030af0fbdf4f21eb2bf2311b3fa72f6b7afdbf72
shangxinbo <shangxb@startimes.com.cn>
2019-10-12 08:36:52 +0800

    player

---
+++ pages/browser/player.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/lJQIoiW2KBs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'default',
+    head() {
+        return {
+            title: 'StarTimes player'
+        }
+    }
+}
+</script>
