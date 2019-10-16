b523fdda66185758e16791c9a907695557f73642
naheya <nahy@startimes.com.cn>
2019-10-16 10:23:43 +0800

    修改按钮设计图

--- pages/hybrid/dvb/reward.vue
+++ pages/hybrid/dvb/reward.vue
@@ -24,31 +24,23 @@ export default {
         }
     },
     methods: {
-        goToLink() {
+        mSendEvLog(action, value) {
             this.sendEvLog({
                 category: "Decoder Users' Gift",
-                action: 'NG-LINK',
+                action: action,
                 label: 1,
-                value: 1
+                value: value
             })
+        },
+        goToLink() {
+            this.mSendEvLog('NG-LINK', 1)
             toNativePage('com.star.mobile.video.dvbservice.DvbServiceActivity')
         },
         toPup() {
-            this.sendEvLog({
-                category: "Decoder Users' Gift",
-                action: 'NG-TOPUP',
-                label: 1,
-                value: 1
-            })
+            this.mSendEvLog('NG-TOPUP', 1)
         },
         getReward() {
             if (this.getRewarding) return false
-            this.sendEvLog({
-                category: "Decoder Users' Gift",
-                action: 'NG-GET_REWARD',
-                label: 1,
-                value: 1
-            })
             this.getRewarding = true
             this.$axios
                 .post('/self/v1/gift-award/after-link')
@@ -56,18 +48,25 @@ export default {
                     this.getRewarding = false
                     const data = res.data
                     if (data.code === 200) {
+                        this.mSendEvLog('NG-GET_REWARD', 1)
                         this.$alert("Congratulations! Redeem successful. ₦300 bonus has been recharge to the decoder account you've linked with.")
                     } else if (data.code == '402') {
+                        this.mSendEvLog('NG-GET_REWARD', 402)
                         this.$alert('Oops, redeem failed. You can get ₦300 bonus only by linking decoder first.')
                     } else if (data.code == '201') {
+                        this.mSendEvLog('NG-GET_REWARD', 201)
                         this.$alert('Your StarTimes ON account has redeemed before. Every account can only get ₦300 bonus for once.')
                     } else if (data.code == '202') {
+                        this.mSendEvLog('NG-GET_REWARD', 202)
                         this.$alert('Your decoder account has redeemed before. Every decoder can only get ₦300 bonus for once.')
                     } else if (data.code == '203') {
+                        this.mSendEvLog('NG-GET_REWARD', 203)
                         this.$alert('Your phone has redeemed before. Every device can only get ₦300 bonus for once.')
                     } else if (data.code == '205') {
+                        this.mSendEvLog('NG-GET_REWARD', 205)
                         this.$alert('Oops, redeem failed. The bonus is only available in Nigeria.')
                     } else if (data.code == '403') {
+                        this.mSendEvLog('NG-GET_REWARD', 403)
                         this.$alert('Smartcard exception, please contact call center.')
                     } else {
                         this.$alert('Network erro, please try again. Or contact call center.')
