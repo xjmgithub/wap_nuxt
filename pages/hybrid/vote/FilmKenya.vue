0df6a4d644229508a9304fb13b7ff99d0cfd31a1
shangxinbo <shangxb@startimes.com.cn>
2019-11-01 18:45:49 +0800

    弹窗会放大

--- pages/hybrid/vote/FilmKenya.vue
+++ pages/hybrid/vote/FilmKenya.vue
@@ -145,6 +145,8 @@ export default {
             if (nv) {
                 document.body.style.overflow = 'hidden'
                 document.body.style.position = 'fixed'
+                document.body.style.left = 0
+                document.body.style.right = 0
             } else {
                 document.body.style.overflow = 'auto'
                 document.body.style.position = 'static'
