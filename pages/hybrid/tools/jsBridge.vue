a255f3707422754610e9ed48ba87d13a1a106c34
shangxinbo <shangxb@startimes.com.cn>
2019-11-05 08:46:48 +0800

    添加测试jsbridge tool

---
+++ pages/hybrid/tools/jsBridge.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>{{headerInfo | jsontoString}}</div>
+</template>
+<script>
+export default {
+    layout: 'base',
+    filters: {
+        jsontoString(val) {
+            return JSON.stringify(val)
+        }
+    },
+    data() {
+        return {
+            headerInfo: {}
+        }
+    },
+    mounted() {
+        const jsGetHeadInfo = window.getChannelId && window.getChannelId.jsGetHeadInfo
+        if (jsGetHeadInfo) {
+            this.headerInfo = jsGetHeadInfo()
+            console.log(this.headerInfo)
+        }
+    }
+}
+</script>
