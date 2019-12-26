ff98ec914e92bb94934269bf5f8b72d976998956
shangxinbo <shangxb@startimes.com.cn>
2019-12-26 13:50:52 +0800

    url link

--- functions/app.js
+++ functions/app.js
@@ -139,7 +139,7 @@ export const downApk = function(callback) {
     if (browser.isIos) {
         window.location.href = appleStore
     } else {
-        window.location.href = 'http://private-app.startimes.me/uploads/pkg/file/788/StarTimesON-test-5.21.3.1-develop-5.21.3.1-variants-27676.apk'
+        window.location.href = 'http://private-app.startimes.me/uploads/pkg/file/790/StarTimesON-test-officialWap-5.21.3.1-develop-5.21.3.1-27676.apk'
         // axios.get('/hybrid/api/app/getApk').then(data => {
         //     window.location.href = data.data.data
         // })
